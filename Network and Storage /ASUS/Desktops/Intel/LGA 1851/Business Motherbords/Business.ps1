7a0fa7e3086b4492f56f6509727e3c9ad28e7fd8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 10:50:34 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
@@ -8,5 +8,5 @@ Get-Date
 ####################################################################################
 
 $BusinessEthernet = ""
-$BusinessWiFi = ""
+$BusinessWiFi = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
 $BusinessStorage = ""
