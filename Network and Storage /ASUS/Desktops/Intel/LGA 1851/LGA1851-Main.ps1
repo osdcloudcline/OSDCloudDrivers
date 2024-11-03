7823d7593ded9b9b8264915e2f93bd7c6ea73b17
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 10:27:48 -0500

    Update LGA1851-Main.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/LGA1851-Main.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/LGA1851-Main.ps1
@@ -117,7 +117,7 @@ do
   switch($selection)
   {
 '1'{cls
-    $Business = Invoke-WebRequest("")
+    $Business = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Business.ps1")
     Invoke-Expression $($Business.Content)
     }
 '2'{cls
