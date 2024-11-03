8dd5431f3e18aa77483f5deeffc52e7d5a2c46fd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 18:25:13 -0500

    Update Other.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Other Motherboards/Other.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Other Motherboards/Other.ps1
@@ -1,1 +1,52 @@
+$LGA1851Other = "C:\Logs\OSDCloud\LGA1851\Other.log"
+Start-Transcript
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$OtherEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/Ethernet%20and%20WiFi/RealtekLANDrivers.zip"
+$OtherWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/Ethernet%20and%20WiFi/MediaTek-WiFiDrivers.zip"
+$OtherStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/Storage/IRST-StorageDrivers.zip"
+
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$OtherDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\Other"
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$OtherEthernet = "$OtherDestination\Ethernet"
+$OtherWiFi = "$OtherDestination\WiFi"
+$OtherStorage = "$OtherDestination\Storage"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS LGA 1851 Other Motherboard Ethernet Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $OtherEthernetURL -DestinationDirectory $OtherDestination
+
+Write-Verbose "Acquiring ASUS LGA 1851 Other Motherboard WiFi Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $OtherWiFiURL -DestinationDirectory $OtherDestination
+
+Write-Verbose "ASUS LGA 1851 Other Motherboard Drivers downloaded" -Verbose
+
+Write-Verbose "Processing: ZIP File extraction" -Verbose
+
+Expand-7Zip -ArchiveFileName "$OtherDestination\Realtek-LANDriver.zip" -TargetPath $OtherEthernet  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$OtherDestination\MediaTekWiFiDrivers.zip" -TargetPath $OtherWiFi  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$OtherDestination\IRST-StorageDrivers.zi" -TargetPath $OtherStorage  -ErrorAction SilentlyContinue 
+
+Write-Verbose "ZIP Files extracted successfully" -Verbose
+
+
+Stop-Transcript
