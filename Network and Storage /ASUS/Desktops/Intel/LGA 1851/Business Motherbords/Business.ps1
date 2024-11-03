18a88888eb6acb4682d025226d07a88f1abe65a9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 10:50:05 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
@@ -1,1 +1,12 @@
+$LGA1851Business = "C:\Logs\OSDCloud\LGA1851\Business.log"
+Start-Transcript
 
+Get-Date
+
+####################################################################################
+#                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$BusinessEthernet = ""
+$BusinessWiFi = ""
+$BusinessStorage = ""
