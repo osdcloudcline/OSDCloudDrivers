2b7a893d62da3f27c76b5c19c6254d005f304a9b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 18:44:40 -0500

    Update ProArt.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/ProArt Motherboards/ProArt.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/ProArt Motherboards/ProArt.ps1
@@ -12,7 +12,7 @@ $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 $ProArtEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/ProArt%20Motherboards/Ethernet%20and%20WiFi/Marvell-LANDriver.zip"
 $ProArtWiFi1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/ProArt%20Motherboards/Ethernet%20and%20WiFi/Intel-WiFiDrivers-1.zip"
 $ProArtWiFi2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/ProArt%20Motherboards/Ethernet%20and%20WiFi/Intel-WiFiDrivers-2.zip"
-$ProArtWiFi3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MProArtboards/Ethernet%20and%20WiFi/MediaTek-WiFiDrivers.zip"
+$ProArtWiFi3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/ProArt%20Motherboards/Ethernet%20and%20WiFi/Intel-WiFiDrivers-3.zip"
 $ProArtWiFi4URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MProArtboards/Ethernet%20and%20WiFi/MediaTek-WiFiDrivers.zip"
 $ProArtStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MProArtboards/Storage/IRST-StorageDrivers.zip"
 
