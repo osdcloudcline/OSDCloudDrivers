30c16ba4dee9bcdde40b9c563dfc3a2d90fd91f4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 11:42:16 -0500

    Update CSM.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/CSM Motherboards/CSM.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/CSM Motherboards/CSM.ps1
@@ -9,7 +9,7 @@ $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 #                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
 ####################################################################################
 
-$CSMEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
+$CSMEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/Ethernet%20and%20WiFi/RealtekLANDrivers.zip"
 $CSMWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/Ethernet%20and%20WiFi/MediaTek-WiFiDrivers.zip"
 $CSMStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Storage/IRSTDrivers.zip"
 
