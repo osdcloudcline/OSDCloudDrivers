c62b668014de89fa017b7775d28003d408dd44b2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-02 22:33:54 -0400

    Update ALL-LGA1851.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
@@ -28,5 +28,5 @@ Import-Module -Name OSD -Force
 ## ASUS LGA 1851 - Business Motherboard URLs
 ############################################################################################################
 
-$BusinessLANURL = ""
+$BusinessLANURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
 $BusinessWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
