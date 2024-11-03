59f628a243afe68c6dcf15ae56d2400bb5584ba8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 11:48:21 -0500

    Update ALL-LGA1851.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
@@ -6,3 +6,6 @@ Get-Date
 
 $BusinessLGA1851 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Business.ps1")
 Invoke-Expression $($BusinessLGA1851.Content)
+
+$CSMLGA1851 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/CSM.ps1")
+Invoke-Expression $($CSMLGA1851.Content)
