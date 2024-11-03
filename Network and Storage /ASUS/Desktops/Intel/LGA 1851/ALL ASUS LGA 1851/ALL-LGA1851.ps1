9e7254275f31d18fc0c9ba61b165f4c5ef928694
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 11:31:53 -0500

    Update ALL-LGA1851.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/ALL ASUS LGA 1851/ALL-LGA1851.ps1
@@ -1,42 +1,8 @@
-Function Get-OSDCloudMount(){
-$OSDCloudMountQues = Read-Host -Prompt 'Is the OSD Cloud boot.wim already mounted?'
-If(($OSDCloudMountQues -eq "Y") -or ($OSDCloudMountQues -eq "y") -or ($OSDCloudMountQues -eq "Yes") -or ($OSDCloudMountQues -eq "yes")){
-$OSDCloudMountDir = Read-Host -Prompt 'Please provide current mount directory'
-}
-ElseIf(($OSDCloudMountQues -eq "N") -or ($OSDCloudMountQues -eq "n") -or ($OSDCloudMountQues -eq "No") -or ($OSDCloudMountQues -eq "no")){
-$OSDCloudWIM = Read-Host 'Please Specify the location of your OSDCloud boot.wim file'
-$mountdir = Read-Host 'Please specify the mount point'
-Write-Host "Mounting OSDCloud boot.wim..." 
-Mount-WindowsImage -ImagePath "$OSDCloudWIM\boot.wim" -Index 1 -Path $mountdir
-Write-Host "Mounting completed..."
-}
-}
+$LGA1851 = "C:\Logs\OSDCloud\ASUS\LGA1851.log"
 
+Start-Transcript
 
-Function Get-OSDCloudDismount(){
-$mountdir = Read-Host -Prompt 'Please provide mount directory'
-Dismount-WindowsImage -Path $mountdir -Save
+Get-Date
 
-}
-
-Function Get-LGA1851Business(){
-$BusinessLANURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
-$BusinessWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
-$BusinessStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Storage/IRSTDrivers.zip"
-
-
-}
-Write-Host "Configuring OSD PowerShell Modules..." -ForegroundColor Green
-
-Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck 
-Import-Module -Name OSD -Force
-
-############################################################################################################
-## ASUS LGA 1851 - Business Motherboard URLs
-############################################################################################################
-
-$BusinessLANURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
-$BusinessWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
-$BusinessStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Storage/IRSTDrivers.zip"
-
-
+$BusinessLGA1851 = Invoke-WebRequest("")
+Invoke-Expression $($BusinessLGA1851.Content)
