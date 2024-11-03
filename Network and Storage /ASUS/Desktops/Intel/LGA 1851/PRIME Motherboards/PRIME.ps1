ce947a3dffdac3c2e01873c285e92251f8f9bb2d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 18:37:15 -0500

    Update PRIME.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/PRIME Motherboards/PRIME.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/PRIME Motherboards/PRIME.ps1
@@ -1,1 +1,52 @@
+$LGA1851PRIME = "C:\Logs\OSDCloud\LGA1851\PRIME.log"
+Start-Transcript
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$PRIMEEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MPRIMEboards/Ethernet%20and%20WiFi/RealtekLANDrivers.zip"
+$PRIMEWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MPRIMEboards/Ethernet%20and%20WiFi/MediaTek-WiFiDrivers.zip"
+$PRIMEStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20MPRIMEboards/Storage/IRST-StorageDrivers.zip"
+
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$PRIMEDestination = "C:\OSDCloud\Drivers\MPRIMEboards\ASUS\LGA1851\PRIME"
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$PRIMEEthernet = "$PRIMEDestination\Ethernet"
+$PRIMEWiFi = "$PRIMEDestination\WiFi"
+$PRIMEStorage = "$PRIMEDestination\Storage"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS LGA 1851 PRIME MPRIMEboard Ethernet Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $PRIMEEthernetURL -DestinationDirectory $PRIMEDestination
+
+Write-Verbose "Acquiring ASUS LGA 1851 PRIME MPRIMEboard WiFi Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $PRIMEWiFiURL -DestinationDirectory $PRIMEDestination
+
+Write-Verbose "ASUS LGA 1851 PRIME MPRIMEboard Drivers downloaded" -Verbose
+
+Write-Verbose "Processing: ZIP File extraction" -Verbose
+
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\Realtek-LANDriver.zip" -TargetPath $PRIMEEthernet  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\MediaTekWiFiDrivers.zip" -TargetPath $PRIMEWiFi  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\IRST-StorageDrivers.zi" -TargetPath $PRIMEStorage  -ErrorAction SilentlyContinue 
+
+Write-Verbose "ZIP Files extracted successfully" -Verbose
+
+
+Stop-Transcript
