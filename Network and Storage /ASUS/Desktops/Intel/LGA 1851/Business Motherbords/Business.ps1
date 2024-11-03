e028398aa6a676546dc3538a569f5fa1c1f1d99e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 11:46:40 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
@@ -6,27 +6,27 @@ Get-Date
 $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 
 ####################################################################################
-#                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
+#                   LGA 1851 Ethernet and WiFi Driver URLs
 ####################################################################################
 
 $BusinessEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
 $BusinessWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
-$BusinessStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Storage/IRSTDrivers.zip"
+
 
 
 ####################################################################################
-#             LGA 1851 Ethernet, WiFi and Storage Driver Download Destination
+#             LGA 1851 Ethernet and WiFi Driver Download Destination
 ####################################################################################
 
 $BusinessDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\Business"
 
 ####################################################################################
-#             LGA 1851 Ethernet, WiFi and Storage Driver Extraction Destination
+#             LGA 1851 Ethernet and WiFi Driver Extraction Destination
 ####################################################################################
 
 $BusinessEthernet = "$BusinessDestination\Ethernet"
 $BusinessWiFi = "$BusinessDestination\WiFi"
-$BusinessStorage = "$BusinessDestination\Storage"
+
 
 Import-Module -Name OSD -Force 
 
@@ -38,17 +38,13 @@ Write-Verbose "Acquiring ASUS LGA 1851 Business Motherboard WiFi Drivers from $G
 
 Save-WebFile -SourceUrl $BusinessWiFiURL -DestinationDirectory $BusinessDestination
 
-Write-Verbose "Acquiring ASUS LGA 1851 Business Motherboard Storage Drivers from $GHURL" -Verbose
-
-Save-WebFile -SourceUrl $BusinessStorageURL -DestinationDirectory $BusinessDestination
-
 Write-Verbose "ASUS LGA 1851 Business Motherboard Drivers downloaded" -Verbose
 
 Write-Verbose "Processing: ZIP File extraction" -Verbose
 
 Expand-7Zip -ArchiveFileName "$BusinessDestination\Realtek-LANDriver.zip" -TargetPath $BusinessEthernet  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$BusinessDestination\MediaTekWiFiDrivers.zip" -TargetPath $BusinessWiFi  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$BusinessDestination\IRSTDrivers.zip" -TargetPath $BusinessStorage  -ErrorAction SilentlyContinue 
+
 
 Write-Verbose "ZIP Files extracted successfully" -Verbose
 
