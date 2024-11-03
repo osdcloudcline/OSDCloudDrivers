13cb64bcb2f07d65bb16fc95120ded1169943660
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 10:51:05 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Business Motherbords/Business.ps1
@@ -7,6 +7,6 @@ Get-Date
 #                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
 ####################################################################################
 
-$BusinessEthernet = ""
+$BusinessEthernet = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/Realtek-LANDriver.zip"
 $BusinessWiFi = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Ethernet%20and%20WiFi/MediaTekWiFiDrivers.zip"
 $BusinessStorage = ""
