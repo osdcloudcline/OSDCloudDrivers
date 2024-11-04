df9de3c7242d26435c9f9a8883ee297ef142d7d1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 22:50:45 -0500

    Update ALL-LGA1700.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1700/ALL ASUS LGA 1700/ALL-LGA1700.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1700/ALL ASUS LGA 1700/ALL-LGA1700.ps1
@@ -9,7 +9,7 @@ If(($LGA1700Storage1 -eq $true) -and ($LGA1700Storage2 -eq $true) -and ($LGA1700
 Write-Verbose "ASUS LGA 1851 Storage drivers have been downloaded and expanded" -Verbose
 }
 ElseIf (($LGA1700Storage1 -eq $false) -and ($LGA1700Storage2 -eq $false) -and ($LGA1700Storage3 -eq $false) -and ($LGA1700Storage4 -eq $false)){
-$LGA1700Storage = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Storage/LGA1700-Storage.ps1")
+$LGA1700Storage = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201700/Storage/LGA1700-Storage.ps1")
 Invoke-Expression $($LGA1700Storage.Content)
 }
 }
