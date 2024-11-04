d00d33dc8802ab3bca3d77e54f8eb4204104bc38
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 22:59:57 -0500

    Update LGA1851-Storage.ps1

--- Network and Storage /ASUS/Desktops/Intel/LGA 1851/Storage/LGA1851-Storage.ps1
+++ Network and Storage /ASUS/Desktops/Intel/LGA 1851/Storage/LGA1851-Storage.ps1
@@ -1,1 +1,41 @@
+$LGA1851Storage = "C:\Logs\OSDCloud\LGA1851\LGA1851Storage.log"
+Start-Transcript -Path $LGA1851Storage
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   LGA 1851 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$LGA1851StorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Storage/IRST-StorageDriver.zip"
+
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$LGA1851StorageDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\GenStorage"
+
+####################################################################################
+#             LGA 1851 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$LGA1851StorageDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\GenStorage"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS LGA 1851Motherboard General Storage Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $LGA1851StorageURL -DestinationDirectory $ROGDestination
+
+Write-Verbose "ASUS LGA 1851 Business Motherboard Drivers downloaded" -Verbose
+
+Write-Verbose "Processing: ZIP File extraction" -Verbose
+
+Expand-7Zip -ArchiveFileName "$LGA1851StorageDestination\IRSTDrivers.zip" -TargetPath $LGA1851StorageDestination  -ErrorAction SilentlyContinue 
+
+Write-Verbose "ZIP Files extracted successfully" -Verbose
+
+Stop-Transcript
