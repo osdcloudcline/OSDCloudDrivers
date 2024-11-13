b400c7c091e3b1b817609ed129160d5e48e3f94a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-12 23:18:22 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/Business Motherboards/Business.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/Business Motherboards/Business.ps1
@@ -15,3 +15,25 @@ $BusinessEthernet2URL = ""
 $BusinessStorage1URL = ""
 $BusinessStorage2URL = ""
 
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$BusinessDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Business"
+
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$BusinessEthernet1 = "$BusinessDestination\Ethernet1"
+$BusinessEthernet2 = "$BusinessDestination\Ethernet2"
+$BusinessStorage1 = "$BusinessDestination\Storage1"
+$BusinessStorage2 = "$BusinessDestination\Storage2"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS AM4 Business Motherboard Ethernet Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $BusinessEthernet1URL -DestinationDirectory $BusinessDestination
+Save-WebFile -SourceUrl $BusinessEthernet2URL -DestinationDirectory $BusinessDestination
+
