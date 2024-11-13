94e42e66fb58d0d3581105553b45b4793c59df16
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-13 07:19:44 -0500

    Update PRIME.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/PRIME Motherboards/PRIME.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/PRIME Motherboards/PRIME.ps1
@@ -1,1 +1,85 @@
+$AM4PRIME = "C:\Logs\OSDCloud\AM4\PRIME.log"
+Start-Transcript -Path $AM4PRIME
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   AM4 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$PRIMEEthernetURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Ethernet%20and%20WiFi/PRIME-Intel-i211-LANDrivers.zip"
+$PRIMEEthernetURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Ethernet%20and%20WiFi/PRIME-Realtek-LANDrivers.zip"
+$PRIMEEthernetURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Ethernet%20and%20WiFi/PRIME-Realtek-LANDrivers2.zip"
+
+$PRIMEWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Ethernet%20and%20WiFi/PRIME-WiFiDrivers.zip"
+
+$PRIMEStorageURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Storage/PRIME-NVMeStorageDrivers.zip"
+$PRIMEStorageURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Storage/PRIME-NVMeStorageDrivers2.zip"
+$PRIMEStorageURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Storage/PRIME-SATAStorageDrivers.zip"
+$PRIMEStorageURL4 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/Storage/PRIME-SATAStorageDrivers2.zip"
+
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$PRIMEDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME"
+
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$PRIMEEthernet1 = "$PRIMEDestination\Ethernet1"
+$PRIMEEthernet2 = "$PRIMEDestination\Ethernet2"
+$PRIMEEthernet3 = "$PRIMEDestination\Ethernet3"
+
+$PRIMEWiFi = "$PRIMEDestination\WiFi"
+
+$PRIMEStorage1 = "$PRIMEDestination\Storage1"
+$PRIMEStorage2 = "$PRIMEDestination\Storage2"
+$PRIMEStorage3 = "$PRIMEDestination\Storage3"
+$PRIMEStorage4 = "$PRIMEDestination\Storage4"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS AM4 PRIME Motherboard Ethernet Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $PRIMEEthernetURL1 -DestinationDirectory $PRIMEDestination
+Save-WebFile -SourceUrl $PRIMEEthernetURL2 -DestinationDirectory $PRIMEDestination
+Save-WebFile -SourceUrl $PRIMEEthernetURL3 -DestinationDirectory $PRIMEDestination
+
+
+Write-Verbose "Acquiring ASUS AM4 PRIME Motherboard WiFi Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $PRIMEWiFiURL -DestinationDirectory $PRIMEDestination
+
+Write-Verbose "Acquiring ASUS AM4 PRIME Motherboard Storage Drivers from $GHURL" -Verbose
+
+Save-WebFile -SourceUrl $PRIMEStorageURL1 -DestinationDirectory $PRIMEDestination
+Save-WebFile -SourceUrl $PRIMEStorageURL2 -DestinationDirectory $PRIMEDestination
+Save-WebFile -SourceUrl $PRIMEStorageURL3 -DestinationDirectory $PRIMEDestination
+Save-WebFile -SourceUrl $PRIMEStorageURL4 -DestinationDirectory $PRIMEDestination
+
+Write-Verbose "ASUS AM4 PRIME Motherboard Drivers downloaded" -Verbose
+
+Write-Verbose "Processing: ZIP File extraction" -Verbose
+
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-Intel-i211-LANDrivers.zip" -TargetPath $PRIMEEthernet  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-Realtek-LANDrivers.zip" -TargetPath $PRIMEEthernet  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-Realtek-LANDrivers2.zip" -TargetPath $PRIMEEthernet  -ErrorAction SilentlyContinue 
+
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-WiFiDrivers.zip" -TargetPath $PRIMEWiFi  -ErrorAction SilentlyContinue 
+
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-NVMeStorageDrivers.zip" -TargetPath $PRIMEStorage  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-NVMeStorageDrivers2.zip" -TargetPath $PRIMEStorage  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-SATAStorageDrivers.zip" -TargetPath $PRIMEStorage  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$PRIMEDestination\PRIME-SATAStorageDrivers2.zip" -TargetPath $PRIMEStorage  -ErrorAction SilentlyContinue 
+
+Write-Verbose "ZIP Files extracted successfully" -Verbose
+
+
+Stop-Transcript
+
+$ASUSOSDCloudISOMain = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASUS/Motherboards/CreateISO-Main.ps1")
+Invoke-Expression $($ASUSOSDCloudISOMain.Content)
