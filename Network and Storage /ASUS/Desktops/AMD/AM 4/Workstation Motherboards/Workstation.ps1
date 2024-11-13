ea416ea0b0900b4bdc51a352fed3862d6b785e81
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-13 08:45:54 -0500

    Update Workstation.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/Workstation Motherboards/Workstation.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/Workstation Motherboards/Workstation.ps1
@@ -1,1 +1,86 @@
+$AM4Workstation = "C:\Logs\OSDCloud\AM4\Workstation.log"
+Start-Transcript -Path $AM4Workstation
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   AM4 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
+
+$WorkstationEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Workstation%20Motherboards/Ethernet%20and%20WiFi/Workstation-Realtek-LANDrivers.zip"
+
+
+$WorkstationStorageURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Workstation%20Motherboards/Storage/Workstation-NVMe-StorageDrivers.zip"
+$WorkstationStorageURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Workstation%20Motherboards/Storage/Workstation-RAID-SATA-StorageDrivers.zip"
+
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Download Destination
+####################################################################################
+
+$WorkstationDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation"
+
+####################################################################################
+#             AM4 Ethernet, WiFi and Storage Driver Extraction Destination
+####################################################################################
+
+$WorkstationEthernet = "$WorkstationDestination\Ethernet"
+
+$WorkstationStorage1 = "$WorkstationDestination\Storage1"
+$WorkstationStorage2 = "$WorkstationDestination\Storage2"
+
+Import-Module -Name OSD -Force 
+
+Write-Verbose "Acquiring ASUS AM4 ROG - Republic of Gamers Motherboard Ethernet Drivers from $GHURL" -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $WorkstationEthernetURL -DestinationDirectory $WorkstationDestination
+
+
+Write-Verbose "Acquiring ASUS AM4 ROG - Republic of Gamers Motherboard Storage Drivers from $GHURL" -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $WorkstationStorageURL1 -DestinationDirectory $WorkstationDestination
+Save-WebFile -SourceUrl $WorkstationStorageURL2 -DestinationDirectory $WorkstationDestination
+
+Write-Verbose "ASUS AM4 ROG - Republic of Gamers Motherboard Drivers downloaded" -Verbose
+Write-Host
+
+Write-Verbose "Processing: ZIP File extraction" -Verbose
+Write-Host
+
+Expand-7Zip -ArchiveFileName "$WorkstationDestination\Workstation-Realtek-LANDrivers.zip" -TargetPath $WorkstationEthernet  -ErrorAction SilentlyContinue 
+
+Expand-7Zip -ArchiveFileName "$WorkstationDestination\Workstation-NVMe-StorageDrivers.zip" -TargetPath $WorkstationStorage1  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$WorkstationDestination\Workstation-RAID-SATA-StorageDrivers.zip" -TargetPath $WorkstationStorage2  -ErrorAction SilentlyContinue 
+
+
+Write-Verbose "ZIP Files extracted successfully" -Verbose
+
+
+Stop-Transcript
+
+$AM4VirtualDrivers = "C:\Logs\OSDCloud\ASUS\AM4Virtual.log"
+
+Start-Transcript -Path $AM4VirtualDrivers
+
+Write-Verbose "Processing: VMWare ESXI Virtualization Drivers" -Verbose
+Write-Host
+
+Write-Verbose "Processing: Hyper-V Virtualization Drivers" -Verbose
+Write-Host
+
+Write-Verbose "Processing: VMWare Workstation Professional Virtualization Drivers" -Verbose
+Write-Host
+
+Write-Verbose "Processing: Proxmox Virtual I/O Virtualization Drivers" -Verbose
+Write-Host
+
+Write-Verbose "Processing: Docker Virtualization Drivers" -Verbose
+Write-Host
+
+Stop-Transcript
+
+$ASUSOSDCloudISOMain = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASUS/Motherboards/CreateISO-Main.ps1")
+Invoke-Expression $($ASUSOSDCloudISOMain.Content)
