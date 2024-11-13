4f219605a5754984dd12d91c2bc4e1a66816f584
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-12 22:53:10 -0500

    Update ALL-ASUS-AM4.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/ALL ASUS AM4/ALL-ASUS-AM4.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/ALL ASUS AM4/ALL-ASUS-AM4.ps1
@@ -30,3 +30,6 @@ Write-Host
 
 $BusinessAM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Business%20Motherboards/Business.ps1")
 Invoke-Expression $($BusinessAM4.Content)
+
+Get-AM4Storage
+
