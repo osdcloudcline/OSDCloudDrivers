683f09a8270233570906989c3d0008b1baad1849
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-12 23:13:17 -0500

    Update Business.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/Business Motherboards/Business.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/Business Motherboards/Business.ps1
@@ -1,1 +1,10 @@
+$AM4Business = "C:\Logs\OSDCloud\ASUS\AM4\Business.log"
+Start-Transcript -Path $AM4Business
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   AM4 Ethernet, WiFi and Storage Driver URLs
+####################################################################################
