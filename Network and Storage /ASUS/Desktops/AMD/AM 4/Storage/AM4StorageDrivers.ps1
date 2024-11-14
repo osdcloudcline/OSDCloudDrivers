586df80b1a15292e0f7d010647a866e6dbc9d5df
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-14 01:01:09 -0500

    Update AM4StorageDrivers.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 4/Storage/AM4StorageDrivers.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 4/Storage/AM4StorageDrivers.ps1
@@ -1,1 +1,25 @@
+$AM4Storage = "C:\Logs\OSDCloud\ASUS\AM4StorageDownload.log"
 
+Start-Transcript -Path $AM4Storage
+
+Get-Date
+
+Write-Verbose "Processing: ASUS AMD Socket AM4 Motherboard Storage Drivers" -Verbose
+Write-Host
+
+Import-Module -Name OSD -Force
+
+$Storage1 = ""
+$Storage2 = ""
+
+$StorageDestination = ""
+
+Write-Verbose "Processing: ASUS AMD Socket AM4 Motherboard NVMe Storage Drivers" -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $Storage1 -DestinationDirectory $StorageDestination
+
+Write-Verbose "Processing: ASUS AMD Socket AM4 Motherboard SATA Storage Drivers" -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $Storage2 -DestinationDirectory $StorageDestination
