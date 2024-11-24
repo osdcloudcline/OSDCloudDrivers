b44248310277a6ba9e8b9a5b3f58a578aa25cd8c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-24 14:25:30 -0500

    Update AORUS.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM4/AORUS/AORUS.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM4/AORUS/AORUS.ps1
@@ -1,4 +1,4 @@
-$AM4AORUS = "C:\Logs\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AORUS.log"
+$AM4AORUS = "C:\Logs\OSDCloud\Gigabyte\AM4\AORUS.log"
 Start-Transcript -Path $AM4AORUS
 
 Get-Date
@@ -9,7 +9,10 @@ $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 #                   AM4 Bluetooth Driver URLs
 ####################################################################################
 
+$AORUSBluetoothB550URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/AORUS/Bluetooth/Gigabyte-AORUS-Bluetooth-B550Drivers.zip"
+$AORUSBluetoothX570URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/AORUS/Bluetooth/Gigabyte-AORUS-Bluetooth-X570Drivers.zip"
 
+$AORUSDestination =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AORUS"
 
 ####################################################################################
 #                   AM4 Ethernet Driver URLs
