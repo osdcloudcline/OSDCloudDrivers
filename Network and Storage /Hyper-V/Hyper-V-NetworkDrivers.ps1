b52572a591c2d34509417a2eded9bf0c53c5fcf2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-23 23:40:19 -0500

    Update Hyper-V-NetworkDrivers.ps1

--- Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
+++ Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
@@ -8,15 +8,15 @@ Get-Date
 ##########################
 
 $AM4NVMePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage\NVMe"
-$NVMEURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Storage/ASRock-AM4-RAID-NVMe-StorageDrivers.zip"
-$NVMEDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage"
+$HyperVNetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/Hyper-V%20-%20NetworkDrivers.zip"
+$HyperVDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage"
 
 Import-Module -Name OSD -Force
 
 
 Write-Verbose "Processing: Microsoft Hyper-V Network Drivers..." -Verbose
 
-Save-WebFile -SourceUrl $NVMeURL -DestinationDirectory $NVMEDownloadPath
+Save-WebFile -SourceUrl $HyperVNetworkDriversURL -DestinationDirectory $HyperVDownloadPath
 Expand-7zip -ArchiveName "$NVMeDownloadPath\ASRock-AM4-RAID-NVMe-StorageDrivers.zip" -TargetPath $AM4NVMePath -ErrorAction SilentlyContinue
 
 Write-Host
