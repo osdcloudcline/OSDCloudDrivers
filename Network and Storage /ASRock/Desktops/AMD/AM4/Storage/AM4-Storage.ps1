61acc01b92cd691c6e6bd87b4b570d67301d4da6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-23 23:30:36 -0500

    Update AM4-Storage.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM4/Storage/AM4-Storage.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM4/Storage/AM4-Storage.ps1
@@ -15,7 +15,7 @@ $NVMEDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage"
 ##########################
 
 $AM4SATAPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage\SATA"
-$SATAURL = ""
+$SATAURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Storage/ASRock-AM4-RAID-SATA-StorageDrivers.zip"
 $SATADownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage"
 
 Import-Module -Name OSD -Force
