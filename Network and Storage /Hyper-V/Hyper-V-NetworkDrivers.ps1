f6142ce101a3d0aac01c0159e1db354f5dc34724
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-23 23:44:49 -0500

    Update Hyper-V-NetworkDrivers.ps1

--- Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
+++ Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
@@ -7,17 +7,16 @@ Get-Date
 # Hyper-V Network Drivers  Path
 ##########################
 
-$AM4NVMePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage\NVMe"
+$HyperVPath = "C:\OSDCloud\Drivers\Virtualization\HyperV\Network"
 $HyperVNetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/HyperV-NetworkDrivers.zip"
-$HyperVDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage"
+$HyperVDownloadPath = "C:\OSDCloud\Drivers\Virtualization\HyperV"
 
 Import-Module -Name OSD -Force
-
 
 Write-Verbose "Processing: Microsoft Hyper-V Network Drivers..." -Verbose
 
 Save-WebFile -SourceUrl $HyperVNetworkDriversURL -DestinationDirectory $HyperVDownloadPath
-Expand-7zip -ArchiveName "$NVMeDownloadPath\ASRock-AM4-RAID-NVMe-StorageDrivers.zip" -TargetPath $AM4NVMePath -ErrorAction SilentlyContinue
+Expand-7zip -ArchiveName "$HyperVDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $HyperVPath -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: Microsoft Hyper-V Network Drivers..." -Verbose
