c44cb10869aaba4ac2c2e4faae8b929b748ac362
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-24 01:25:18 -0500

    Update ALL-ASRock-AM4.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM4/ALL ASRock AM4/ALL-ASRock-AM4.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM4/ALL ASRock AM4/ALL-ASRock-AM4.ps1
@@ -293,7 +293,7 @@ Write-Host
 Write-Verbose "Processing: ASRock AM4 Fatal1ty Gaming Motherboards" -Verbose
 Write-Host
 
-$Fatal1tyGamingAM4 = Invoke-WebRequest("")
+$Fatal1tyGamingAM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Fatal1tyGaming.ps1")
 Invoke-Expression $($Fatal1tyGamingAM4.Content)
 
 Get-AM4Storage
