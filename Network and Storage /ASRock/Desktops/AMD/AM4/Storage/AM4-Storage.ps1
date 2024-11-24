e709a4c8b89cfa0a522630fb21f82d4cda800a13
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-23 21:02:45 -0500

    Update AM4-Storage.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM4/Storage/AM4-Storage.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM4/Storage/AM4-Storage.ps1
@@ -1,27 +1,36 @@
 
 
 
-
-
 #########################
 # AM4 NVMe Storage Path
 ##########################
 
 $AM4NVMePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage\NVMe"
+$NVMEURL = ""
+$NVMEDownloadPath = ""
+
 
 ###########################
 # AM4 SATA Storage Path 
 ##########################
 
 $AM4SATAPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Storage\SATA"
+$SATAURL = ""
+$SATADownloadPath = ""
 
 Import-Module -Name OSD -Force
 
+
 Write-Verbose "Processing: ASRock AM4 NVMe Storage Drivers..." -Verbose
 
-$NVMEURL = ""
-$NVMEDownloadPath = ""
-$NVMEExtractPath = ""
-
 Save-WebFile -SourceUrl $NVMeURL -DestinationDirectory $NVMEDownloadPath
+Extract-7zip 
+
+Write-Host
+Write-Verbose "Completed: ASRock AM4 NVMe Storage Drivers..." -Verbose
+Write-Host
+
+Write-Verbose "Processing: ASRock AM4 SATA Storage Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $SATAURL -DestinationDirectory $SATADownloadPath
 Extract-7zip 
