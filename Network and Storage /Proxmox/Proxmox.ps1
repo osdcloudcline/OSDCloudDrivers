84e99efaae1329d55abe0e1355f32eecd91609ba
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-24 09:37:23 -0500

    Update Proxmox.ps1

--- Network and Storage /Proxmox/Proxmox.ps1
+++ Network and Storage /Proxmox/Proxmox.ps1
@@ -10,8 +10,8 @@ Get-Date
 
 $ProxmoxPath1 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Network\Windows11"
 $ProxmoxPath2 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Network\WindowsServer2025"
-$ProxmoxVNetworkDriversURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Wi11-Ethernet.zip"
-$ProxmoxVNetworkDriversURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/HyperV-NetworkDrivers.zip"
+$ProxmoxNetworkDriversURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Wi11-Ethernet.zip"
+$ProxmoxNetworkDriversURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-WinServer2025-Ethernet.zip"
 $ProxmoxDownloadPath = "C:\OSDCloud\Drivers\Virtualization\Proxmox"
 
 Import-Module -Name OSD -Force
@@ -22,7 +22,7 @@ Save-WebFile -SourceUrl $ProxmoxNetworkDriversURL1 -DestinationDirectory $Proxmo
 Expand-7zip -ArchiveName "$ProxmoxDownloadPath\Proxmox-Wi11-Ethernet.zip" -TargetPath $ProxmoxPath1 -ErrorAction SilentlyContinue
 
 Save-WebFile -SourceUrl $ProxmoxNetworkDriversURL2 -DestinationDirectory $ProxmoxDownloadPath
-Expand-7zip -ArchiveName "$ProxmoxDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $ProxmoxPath2 -ErrorAction SilentlyContinue
+Expand-7zip -ArchiveName "$ProxmoxDownloadPath\Proxmox-WinServer2025-Ethernet.zip" -TargetPath $ProxmoxPath2 -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: Proxmox Network Drivers..." -Verbose
@@ -40,16 +40,21 @@ Get-Date
 # Proxmox Storage Drivers  Path
 ##########################
 
-$ProxmoxPath = "C:\OSDCloud\Drivers\Virtualization\HyperV\Network"
-$ProxmoxVNetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/HyperV-NetworkDrivers.zip"
-$ProxmoxDownloadPath = "C:\OSDCloud\Drivers\Virtualization\HyperV"
+$ProxmoxPath1 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\Windows11"
+$ProxmoxPath2 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\WindowsServer2025"
+$ProxmoxStorageDriversURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Wi11-Ethernet.zip"
+$ProxmoxStorageDriversURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-WinServer2025-Ethernet.zip"
+$ProxmoxDownloadPath = "C:\OSDCloud\Drivers\Virtualization\Proxmox"
 
 Import-Module -Name OSD -Force
 
-Write-Verbose "Processing: Proxmox Network Drivers..." -Verbose
+Write-Verbose "Processing: Proxmox Storage Drivers..." -Verbose
 
-Save-WebFile -SourceUrl $ProxmoxNetworkDriversURL -DestinationDirectory $ProxmoxDownloadPath
-Expand-7zip -ArchiveName "$ProxmoxDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $HyperVPath -ErrorAction SilentlyContinue
+Save-WebFile -SourceUrl $ProxmoxStorageDriversURL1 -DestinationDirectory $ProxmoxDownloadPath
+Expand-7zip -ArchiveName "$ProxmoxDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $ProxmoxPath1 -ErrorAction SilentlyContinue
+
+Save-WebFile -SourceUrl $ProxmoxStorageDriversURL1 -DestinationDirectory $ProxmoxDownloadPath
+Expand-7zip -ArchiveName "$ProxmoxDownloadPath\Proxmox-WinServer2025-Ethernet.zip" -TargetPath $ProxmoxPath2 -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: Proxmox Network Drivers..." -Verbose
