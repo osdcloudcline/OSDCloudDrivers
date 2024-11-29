30c5723eedceabee8c0e168e5c7a487606985b70
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-29 00:47:10 -0500

    Update UltraDurable.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM4/Ultra Durable/UltraDurable.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM4/Ultra Durable/UltraDurable.ps1
@@ -40,7 +40,7 @@ $UDEthernetURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/hea
 $UDEthernetURL4 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B450-Win10.zip"
 $UDEthernetURL5 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B450-Win11.zip"
 
-# Ultra Durable B550
+# Ultra Durable - B550
 
 $UDEthernetURL6 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B550-Intel-1.zip"
 $UDEthernetURL7 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B550-Intel-2.zip"
@@ -51,12 +51,12 @@ $UDEthernetURL11 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/he
 $UDEthernetURL12 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B550-Realtek-Win10.zip"
 $UDEthernetURL13 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-B550-Realtek-Win11.zip"
 
-# Ultra Durable X370
+# Ultra Durable -  X370
 
 $UDEthernetURL14 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-X370-Realtek-Win10.zip"
 
 
-# Ultra Durable X570
+# Ultra Durable -  X570
 
 $UDEthernetURL15 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-X570-Realtek-Win10.zip"
 $UDEthernetURL16 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/Ethernet/Gigabyte-UltraDurable-EthernetDrivers-X570-Realtek-Win11.zip"
@@ -65,10 +65,20 @@ $UDEthernetURL16 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/he
 #                   AM4 WiFi Driver URLs
 ####################################################################################
 
+# Ultra Durable - A320
 
+$UDWiFiURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-A320-WiFiDrivers-Intel.zip"
+$UDWiFiURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-A320-WiFiDrivers-Realtek.zip"
 
+# Ultra Durable - B450
 
+$UDWiFiURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-B450-WiFiDrivers-Intel.zip"
+$UDWiFiURL4 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-B450-WiFiDrivers-Realtek.zip"
 
+# Ultra Durable - B550
+
+$UDWiFiURL5 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-B550-WiFiDrivers-Intel.zip"
+$UDWiFiURL6 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/WiFi/Gigabyte-UltraDurable-B550-WiFiDrivers-Realtek.zip"
 
 ####################################################################################
 #                   AM4 Storage Driver URLs
@@ -122,4 +132,81 @@ $UDStorageURL21 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/hea
 ###########################################################################################
 
 $UDDestination =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\Ultra Durable"
+
+###########################################################################################
+#        Ultra Durable Ethernet, Bluetooth, Storage and WiFi Extraction Folders
+###########################################################################################
+
+# Bluetooth
+
+$UDBluetooth1 = "$UDDestination\B550\Bluetooth1"
+$UDBluetooth2 = "$UDDestination\A520\Bluetooth1"
+$UDBluetooth3 = "$UDDestination\B450\Bluetooth1"
+
+# Ethernet
+
+$UDEthernet1 = "$UDDestination\A520\Ethernet1"
+$UDEthernet2 = "$UDDestination\A520\Ethernet2"
+
+$UDEthernet3 = "$UDDestination\B350\Ethernet"
+
+
+$UDEthernet4 = "$UDDestination\B450\Ethernet1"
+$UDEthernet5 = "$UDDestination\B450\Ethernet2"
+
+$UDEthernet6 = "$UDDestination\B550\Ethernet1"
+$UDEthernet7 = "$UDDestination\B550\Ethernet2"
+$UDEthernet8 = "$UDDestination\B550\Ethernet3"
+$UDEthernet9 = "$UDDestination\B550\Ethernet4"
+$UDEthernet10 = "$UDDestination\B550\Ethernet5"
+$UDEthernet11 = "$UDDestination\B550\Ethernet6"
+$UDEthernet12 = "$UDDestination\B550\Ethernet7"
+$UDEthernet13 = "$UDDestination\B550\Ethernet8"
+
+$UDEthernet14 = "$UDDestination\X370\Ethernet"
+
+$UDEthernet15 = "$UDDestination\X570\Ethernet1"
+$UDEthernet16 = "$UDDestination\X570\Ethernet1"
+
+
+# Storage
+
+$UDStorage1 = "$UDDestination\A320\RAID\NVMe"
+$UDStorage2 = "$UDDestination\A320\RAID\SATA"
+
+$UDStorage3 = "$UDDestination\A520\RAID\NVMe1"
+$UDStorage4 = "$UDDestination\A520\RAID\NVMe2"
+$UDStorage5 = "$UDDestination\A520\RAID\NVMe3"
+$UDStorage6 = "$UDDestination\A520\RAID\NVMe4"
+$UDStorage7 = "$UDDestination\A520\RAID\SATA1"
+$UDStorage8 = "$UDDestination\A520\RAID\SATA2"
+
+$UDStorage9 = "$UDDestination\B350\RAID\NVMe"
+$UDStorage10 = "$UDDestination\B350\RAID\SATA"
+
+$UDStorage11 = "$UDDestination\B450\RAID\NVMe"
+$UDStorage12 = "$UDDestination\B450\RAID\SATA"
+
+$UDStorage13 = "$UDDestination\B550\RAID\NVMe1"
+$UDStorage14 = "$UDDestination\B550\RAID\NVMe2"
+$UDStorage15 = "$UDDestination\B550\RAID\SATA"
+$UDStorage16 = "$UDDestination\B550\RAID\Thunderbolt"
+
+$UDStorage17 = "$UDDestination\X370\RAID\NVMe"
+$UDStorage18 = "$UDDestination\X370\RAID\SATA"
+
+
+$UDStorage19 = "$UDDestination\X570\RAID\NVMe1"
+$UDStorage20 = "$UDDestination\X570\RAID\NVMe2"
+$UDStorage21 = "$UDDestination\Storage\X570\RAID\SATA"
+
+
+# WiFi 
+
+$UDWiFi1 = "$UDDestination\A320\WiFi1"
+$UDWiFi2 = "$UDDestination\A320\WiFi2"
+$UDWiFi3 = "$UDDestination\B450\WiFi1"
+$UDWiFi4 = "$UDDestination\B450\WiFi2"
+$UDWiFi5 = "$UDDestination\B550\WiFi1"
+$UDWiFi6 = "$UDDestination\B550\WiFi2"
 
