2076749c2358db2e6c1c0a292b67c5bbde7c19e2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-02 22:24:59 -0500

    Update A520.ps1

--- Chipset Drivers/Socket AM4/A520.ps1
+++ Chipset Drivers/Socket AM4/A520.ps1
@@ -8,7 +8,7 @@ $ChipsetSource1 = "https://drivers.amd.com/drivers/amd_chipset_software_6.10.17.
 $ChipsetSource2 = "https://drivers.amd.com/drivers/amd_raid_software_2.20.19.037.exe"
 $ChipsetSource3 = "https://download.amd.com/software/AMD_StoreMI.zip"
 
-$Destination = "C:\OSDCloud\drivers\Chipsets\AM4\A320"
+$Destination = "C:\OSDCloud\drivers\Chipsets\AM4\A520"
 
 Write-Verbose "Processing: $Chipset1..." -Verbose
 Save-WebFile -SourceUrl $ChipsetSource1 -DestinationDirectory $Destination
@@ -18,3 +18,4 @@ Save-WebFile -SourceUrl $ChipsetSource2 -DestinationDirectory $Destination
 
 Write-Verbose "Processing: $Chipset3..." -Verbose
 Save-WebFile -SourceUrl $ChipsetSource3 -DestinationDirectory $Destination
+Expand-Archive -Path "$Destination\AMD_StoreMI.zip" -DestinationPath "C:\OSDCloud\drivers\Chipsets\AM4\A520\StoreMI"
