9dfd91696124ff0fb5c1dbafaa7405b2f0c27cc7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-12 08:42:24 -0500

    Update ALL-ASRock-AM5.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM5/ALL-ASRock-AM5/ALL-ASRock-AM5.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM5/ALL-ASRock-AM5/ALL-ASRock-AM5.ps1
@@ -299,3 +299,5 @@ Write-Host
 
 $TaichiAM5 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Taichi%20Motherboards/Taichi.ps1")
 Invoke-Expression $($TaichiAM5.Content)
+
+Stop-Transcript
