a253f47f6ece91c7bd8954b8ad1bf5e0ed334544
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:27:56 -0500

    Update ESXI-StorageDrivers.ps1

--- Network and Storage /VMWare ESXI/ESXI-StorageDrivers.ps1
+++ Network and Storage /VMWare ESXI/ESXI-StorageDrivers.ps1
@@ -1,1 +1,26 @@
+$ESXIStorageLog = "C:\Logs\OSDCloud\ESXI\StorageDrivers.log"
 
+Start-Transcript -Path $ESXIStorageLog
+
+Get-Date
+
+#########################
+# ESXI Storage Drivers Path
+##########################
+
+$ESXIPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Storage"
+$ESXIStorageDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$ESXIDownloadPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Storage"
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: VMWare ESXI Storage Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $ESXIStorageDriversURL -DestinationDirectory $ESXIDownloadPath2
+Expand-7zip -ArchiveName "$ESXIDownloadPath\ESXIDrivers.zip" -TargetPath $ESXIPath2 -ErrorAction SilentlyContinue
+
+Write-Host
+Write-Verbose "Completed: ESXI Storage Drivers..." -Verbose
+Write-Host
+
+Stop-Transcript
