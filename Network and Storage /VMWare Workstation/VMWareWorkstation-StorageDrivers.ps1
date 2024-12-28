571823741b74e878266886123fb6f4d4ce1657d5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:37:38 -0500

    Update VMWareWorkstation-StorageDrivers.ps1

--- Network and Storage /VMWare Workstation/VMWareWorkstation-StorageDrivers.ps1
+++ Network and Storage /VMWare Workstation/VMWareWorkstation-StorageDrivers.ps1
@@ -1,1 +1,26 @@
+$VMWareWSPROStorageLog = "C:\Logs\OSDCloud\VMWareWSPRO\StorageDrivers.log"
 
+Start-Transcript -Path $VMWareWSPROStorageLog
+
+Get-Date
+
+#########################
+# VMWareWSPRO Storage Drivers Path
+##########################
+
+$VMWareWSPROPath2 = "C:\OSDCloud\Drivers\Virtualization\VMWareWSPRO\Storage"
+$VMWareWSPROStorageDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$VMWareWSPRODownloadPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Storage"
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: VMWare Workstation Professional Storage Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $VMWareWSPROStorageDriversURL -DestinationDirectory $VMWareWSPRODownloadPath2
+Expand-7zip -ArchiveName "$ESXIDownloadPath\ESXIDrivers.zip" -TargetPath $VMWareWSPROPath2 -ErrorAction SilentlyContinue
+
+Write-Host
+Write-Verbose "Completed: VMWare Workstation Professional Storage Drivers..." -Verbose
+Write-Host
+
+Stop-Transcript
