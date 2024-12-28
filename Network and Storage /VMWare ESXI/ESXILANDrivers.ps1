e6a941c0381c10a2dd40987ab06c59a7379eac1c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:28:18 -0500

    Update ESXILANDrivers.ps1

--- Network and Storage /VMWare ESXI/ESXILANDrivers.ps1
+++ Network and Storage /VMWare ESXI/ESXILANDrivers.ps1
@@ -1,1 +1,26 @@
+$ESXINetLog = "C:\Logs\OSDCloud\ESXI\NetworkDrivers.log"
 
+Start-Transcript -Path $ESXINetLog
+
+Get-Date
+
+#########################
+# ESXI Network Drivers  Path
+##########################
+
+$ESXIPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Network"
+$ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$ESXIDownloadPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Network"
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: VMWare ESXI Network Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $ESXINetworkDriversURL -DestinationDirectory $ESXIDownloadPath1
+Expand-7zip -ArchiveName "$ESXIDownloadPath\ESXIDrivers.zip" -TargetPath $ESXIPath1 -ErrorAction SilentlyContinue
+
+Write-Host
+Write-Verbose "Completed: ESXI Network Drivers..." -Verbose
+Write-Host
+
+Stop-Transcript
