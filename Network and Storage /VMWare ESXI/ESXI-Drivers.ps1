aaadb8307eb5ad29035567e672dc8201bf3d5627
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:27:43 -0500

    Update ESXI-Drivers.ps1

--- Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
+++ Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
@@ -9,18 +9,45 @@ Get-Date
 ##########################
 
 $ESXIPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Network"
-$ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXILANDrivers.ps1"
-$ESXIDownloadPath = "C:\OSDCloud\Drivers\Virtualization\ESXI"
+$ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$ESXIDownloadPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Network"
 
 Import-Module -Name OSD -Force
 
 Write-Verbose "Processing: VMWare ESXI Network Drivers..." -Verbose
 
-Save-WebFile -SourceUrl $ESXINetworkDriversURL -DestinationDirectory $ESXIDownloadPath
-Expand-7zip -ArchiveName "$ESXIDownloadPath\Proxmox-Wi11-Ethernet.zip" -TargetPath $ESXIPath1 -ErrorAction SilentlyContinue
+Save-WebFile -SourceUrl $ESXINetworkDriversURL -DestinationDirectory $ESXIDownloadPath1
+Expand-7zip -ArchiveName "$ESXIDownloadPath\ESXIDrivers.zip" -TargetPath $ESXIPath1 -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: ESXI Network Drivers..." -Verbose
+Write-Host
+
+Stop-Transcript
+
+$ESXIStorageLog = "C:\Logs\OSDCloud\ESXI\StorageDrivers.log"
+
+Start-Transcript -Path $ESXIStorageLog
+
+Get-Date
+
+#########################
+# ESXI Storage Drivers Path
+##########################
+
+$ESXIPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Storage"
+$ESXIStorageDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$ESXIDownloadPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Storage"
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: VMWare ESXI Storage Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $ESXIStorageDriversURL -DestinationDirectory $ESXIDownloadPath2
+Expand-7zip -ArchiveName "$ESXIDownloadPath\ESXIDrivers.zip" -TargetPath $ESXIPath2 -ErrorAction SilentlyContinue
+
+Write-Host
+Write-Verbose "Completed: ESXI Storage Drivers..." -Verbose
 Write-Host
 
 Stop-Transcript
