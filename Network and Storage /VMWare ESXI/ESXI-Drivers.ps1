35c337f92c517acf944f3d186dbc98665f6b99f6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:20:31 -0500

    Update ESXI-Drivers.ps1

--- Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
+++ Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
@@ -9,7 +9,7 @@ Get-Date
 ##########################
 
 $ESXIPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Network"
-$ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Wi11-Ethernet.zip"
+$ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXILANDrivers.ps1"
 $ESXIDownloadPath = "C:\OSDCloud\Drivers\Virtualization\ESXI"
 
 Import-Module -Name OSD -Force
@@ -18,9 +18,6 @@ Write-Verbose "Processing: VMWare ESXI Network Drivers..." -Verbose
 
 Save-WebFile -SourceUrl $ESXINetworkDriversURL -DestinationDirectory $ESXIDownloadPath
 Expand-7zip -ArchiveName "$ESXIDownloadPath\Proxmox-Wi11-Ethernet.zip" -TargetPath $ESXIPath1 -ErrorAction SilentlyContinue
-
-Save-WebFile -SourceUrl $ProxmoxNetworkDriversURL2 -DestinationDirectory $ProxmoxDownloadPath
-Expand-7zip -ArchiveName "$ProxmoxDownloadPath\Proxmox-WinServer2025-Ethernet.zip" -TargetPath $ProxmoxPath2 -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: ESXI Network Drivers..." -Verbose
