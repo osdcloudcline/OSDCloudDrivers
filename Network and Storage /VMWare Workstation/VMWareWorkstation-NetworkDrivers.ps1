37a2fea97a2f2929b08ebb4c32f94ee190c33726
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 18:37:11 -0500

    Update VMWareWorkstation-NetworkDrivers.ps1

--- Network and Storage /VMWare Workstation/VMWareWorkstation-NetworkDrivers.ps1
+++ Network and Storage /VMWare Workstation/VMWareWorkstation-NetworkDrivers.ps1
@@ -1,1 +1,26 @@
+$VMWareWSPRONetLog = "C:\Logs\OSDCloud\VMWSPRO\NetworkDrivers.log"
 
+Start-Transcript -Path $VMWareWSPRONetLog
+
+Get-Date
+
+#########################
+# VMWareWSPRO Network Drivers  Path
+##########################
+
+$VMWareWSPROPath1 = "C:\OSDCloud\Drivers\Virtualization\VMWareWSPRO\Network"
+$VMWareWSPRONetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
+$VMWareWSPRODownloadPath1 = "C:\OSDCloud\Drivers\Virtualization\VMWareWSPRO\Download\Network"
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: VMWare Workstation Professional Network Drivers..." -Verbose
+
+Save-WebFile -SourceUrl $VMWareWSPRONetworkDriversURL -DestinationDirectory $VMWareWSPRODownloadPath1
+Expand-7zip -ArchiveName "$VMWareWSPRODownloadPath1\ESXIDrivers.zip" -TargetPath $VMWareWSPROPath1 -ErrorAction SilentlyContinue
+
+Write-Host
+Write-Verbose "Completed: VMWare Workstation Professional Network Drivers..." -Verbose
+Write-Host
+
+Stop-Transcript
