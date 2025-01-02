424f8763ece7f4c4d64454d5b7037ff1a902a5f6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-02 14:03:02 -0500

    Update UD-Ultra Durable.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM5/UD - Ultra Durable/UD-Ultra Durable.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM5/UD - Ultra Durable/UD-Ultra Durable.ps1
@@ -71,3 +71,48 @@ $UDStorageURL1 = ""
 $UDDestination =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable"
 $UDDestinationA620 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\ZIP\A620"
 $UDDestinationB650 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\ZIP\B650"
+
+
+###########################################################################################
+#        Ultra Durable Ethernet, Bluetooth, Storage and WiFi Extraction Folders
+###########################################################################################
+
+# Ultra Durable A620 - Bluetooth
+
+$UDBluetooth1 = "$UDDestination\A620\Bluetooth1"
+$UDBluetooth2 = "$UDDestination\A620\Bluetooth2"
+
+# Ultra Durable B650 - Bluetooth
+
+$UDBluetooth3 = "$UDDestination\B650\Bluetooth1"
+$UDBluetooth4 = "$UDDestination\B650\Bluetooth2"
+
+# Ultra Durable A620 - Ethernet
+
+$UDEthernet1 = "$UDDestination\A620\Ethernet1"
+$UDEthernet2 = "$UDDestination\A620\Ethernet2"
+
+# Ultra Durable B650 - Ethernet
+
+$UDEthernet3 = "$UDDestination\B650\Ethernet1"
+$UDEthernet4 = "$UDDestination\B650\Ethernet2"
+
+# Ultra Durable A620 - Storage
+
+$UDStorage1 = "$UDDestination\A620\Storage\RAID\NVMe"
+
+# Ultra Durable B650 - Storage
+
+$UDStorage2 = "$UDDestination\B650\Storage\RAID\NVMe"
+
+# Ultra Durable A620 - WiFi
+
+$UDWiFi1 = "$UDDestination\A620\WiFi1"
+$UDWiFi2 = "$UDDestination\A620\WiFi2"
+
+# Ultra Durable B650 - WiFi
+
+$UDWiFi3 = "$UDDestination\B650\WiFi1"
+$UDWiFi4 = "$UDDestination\B650\WiFi2"
+
+
