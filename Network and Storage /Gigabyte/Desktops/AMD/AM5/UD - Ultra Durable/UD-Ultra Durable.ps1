350d4291baf71fb8ebc89a9bb990542421bff17a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-02 13:51:31 -0500

    Update UD-Ultra Durable.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM5/UD - Ultra Durable/UD-Ultra Durable.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM5/UD - Ultra Durable/UD-Ultra Durable.ps1
@@ -1,1 +1,59 @@
+$AM5UltraDurable = "C:\Logs\OSDCloud\Gigabyte\AM5\UD-UltraDurable.log"
+Start-Transcript -Path $AM5UltraDurable
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+####################################################################################
+#                   AM5 Bluetooth Driver URLs
+####################################################################################
+
+# Ultra Durable A620 - Bluetooth
+
+$UDBluetoothURL1 = ""
+$UDBluetoothURL2 = ""
+
+
+# Ultra Durable B650 - Bluetooth
+
+$UDBluetoothURL3 = ""
+$UDBluetoothURL4 = ""
+
+
+
+####################################################################################
+#                   AM5 Ethernet Driver URLs
+####################################################################################
+
+# Ultra Durable A620 - Ethernet
+
+
+
+# Ultra Durable B650 - Ethernet
+
+
+
+
+####################################################################################
+#                   AM5 WiFi Driver URLs
+####################################################################################
+
+# Ultra Durable A620 - WiFi
+
+
+
+# Ultra Durable B650 - WiFi
+
+
+
+
+####################################################################################
+#                   AM5 Storage Driver URLs
+####################################################################################
+
+# Ultra Durable A620 - Storage
+
+
+
+# Ultra Durable B650 - Storage
