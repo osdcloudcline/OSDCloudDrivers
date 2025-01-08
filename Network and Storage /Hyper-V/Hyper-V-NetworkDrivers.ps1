d31a125bb5bebb7c03c1274d60e4fe542c84e144
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-07 23:51:28 -0500

    Update Hyper-V-NetworkDrivers.ps1

--- Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
+++ Network and Storage /Hyper-V/Hyper-V-NetworkDrivers.ps1
@@ -10,9 +10,9 @@ Import-Module -Name OSD -Force
 Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -Verbose
 Import-Module -Name 7Zip4Powershell -Force
 
-#########################
+################################
 # Hyper-V Network Drivers  Path
-##########################
+#################################
 
 $HyperVPath = "C:\OSDCloud\Drivers\Virtualization\HyperV\Network"
 $HyperVNetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/HyperV-NetworkDrivers.zip"
@@ -23,7 +23,7 @@ Import-Module -Name OSD -Force
 Write-Verbose "Processing: Microsoft Hyper-V Network Drivers..." -Verbose
 
 Save-WebFile -SourceUrl $HyperVNetworkDriversURL -DestinationDirectory $HyperVDownloadPath
-Expand-7zip -ArchiveName "$HyperVDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $HyperVPath -ErrorAction SilentlyContinue
+Expand-7zip -ArchiveFileName "$HyperVDownloadPath\HyperV-NetworkDrivers.zip" -TargetPath $HyperVPath -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: Microsoft Hyper-V Network Drivers..." -Verbose
