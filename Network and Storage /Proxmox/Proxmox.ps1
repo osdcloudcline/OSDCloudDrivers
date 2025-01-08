ccdfc567eaab5738e2f7c7ffbd005fb5eaa52e3e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-08 14:34:12 -0500

    Update Proxmox.ps1

--- Network and Storage /Proxmox/Proxmox.ps1
+++ Network and Storage /Proxmox/Proxmox.ps1
@@ -46,21 +46,21 @@ Get-Date
 # Proxmox Storage Drivers  Path
 ##########################
 
-$ProxmoxPath1 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\Windows11"
-$ProxmoxPath2 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\WindowsServer2025"
-$ProxmoxStorageDriversURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Win11-StorageDrivers.zip"
-$ProxmoxStorageDriversURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-WinServer2025-StorageDrivers.zip"
+$ProxmoxPath3 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\Windows11"
+$ProxmoxPath4 = "C:\OSDCloud\Drivers\Virtualization\Proxmox\Storage\WindowsServer2025"
+$ProxmoxStorageDriversURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-Win11-StorageDrivers.zip"
+$ProxmoxStorageDriversURL4 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox-WinServer2025-StorageDrivers.zip"
 $ProxmoxDownloadPath = "C:\OSDCloud\Drivers\Virtualization\Proxmox"
 
 Import-Module -Name OSD -Force
 
 Write-Verbose "Processing: Proxmox Storage Drivers..." -Verbose
 
-Save-WebFile -SourceUrl $ProxmoxStorageDriversURL1 -DestinationDirectory $ProxmoxDownloadPath
-Expand-7zip -ArchiveFileName "$ProxmoxDownloadPath\Proxmox-Win11-StorageDrivers.zip" -TargetPath $ProxmoxPath1 -ErrorAction SilentlyContinue
+Save-WebFile -SourceUrl $ProxmoxStorageDriversURL3 -DestinationDirectory $ProxmoxDownloadPath
+Expand-7zip -ArchiveFileName "$ProxmoxDownloadPath\Proxmox-Win11-StorageDrivers.zip" -TargetPath $ProxmoxPath3 -ErrorAction SilentlyContinue
 
-Save-WebFile -SourceUrl $ProxmoxStorageDriversURL2 -DestinationDirectory $ProxmoxDownloadPath
-Expand-7zip -ArchiveFileName "$ProxmoxDownloadPath\Proxmox-WinServer2025-StorageDrivers.zip" -TargetPath $ProxmoxPath2 -ErrorAction SilentlyContinue
+Save-WebFile -SourceUrl $ProxmoxStorageDriversURL4 -DestinationDirectory $ProxmoxDownloadPath
+Expand-7zip -ArchiveFileName "$ProxmoxDownloadPath\Proxmox-WinServer2025-StorageDrivers.zip" -TargetPath $ProxmoxPath4 -ErrorAction SilentlyContinue
 
 Write-Host
 Write-Verbose "Completed: Proxmox Storage Drivers..." -Verbose
