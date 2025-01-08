b54ee0c1c2e14dd9ecaadf53045b6b028b54a53f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-08 08:36:10 -0500

    Update Expedition.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 5/Expedition Motherboards/Expedition.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 5/Expedition Motherboards/Expedition.ps1
@@ -1,1 +1,9 @@
+$AM5Expedition = "C:\Logs\OSDCloud\ASUS\AM5\Expedition.log"
+Start-Transcript -Path $AM5Expedition
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
