b782ac64fcb8a46593a64d7857907f395c697b7f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-08 08:35:19 -0500

    Update CSM.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 5/CSM Motherboards/CSM.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 5/CSM Motherboards/CSM.ps1
@@ -1,1 +1,9 @@
+$AM5CSM = "C:\Logs\OSDCloud\ASUS\AM5\CSM.log"
+Start-Transcript -Path $AM5CSM
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
