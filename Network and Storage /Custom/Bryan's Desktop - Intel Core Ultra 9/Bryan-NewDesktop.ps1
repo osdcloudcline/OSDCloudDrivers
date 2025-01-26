608088ab993db9bd553b2580b19e4890810e14c9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-26 09:10:30 -0500

    Update Bryan-NewDesktop.ps1

--- Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
+++ Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
@@ -1,1 +1,30 @@
+$BryanNewDesktopLog = "C:\Logs\OSDCloud\Custom\Desktop\Bryan-ASUS-Z890-A-STRIX.log"
 
+Start-Transcript -Path $BryanNewDesktopLog
+
+Get-Date
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+################################################
+# Bryan ASUS Z890-A STRIX Ethernet Drivers  Path
+################################################
+
+
+
+################################################
+# Bryan ASUS Z890-A STRIX WiFi Drivers  Path
+################################################
+
+
+
+################################################
+# Bryan ASUS Z890-A STRIX Bluetooth Drivers  Path
+################################################
+
+
+
+################################################
+# Bryan ASUS Z890-A STRIX Storage Drivers  Path
+################################################
