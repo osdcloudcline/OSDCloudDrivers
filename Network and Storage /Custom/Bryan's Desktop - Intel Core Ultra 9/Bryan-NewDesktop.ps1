759371091723913ba173f6ce4537e3ab8a8136e6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-26 09:15:33 -0500

    Update Bryan-NewDesktop.ps1

--- Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
+++ Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
@@ -11,7 +11,7 @@ Import-Module -Name 7Zip4Powershell -Force
 # Bryan ASUS Z890-A STRIX Ethernet Drivers URL
 ################################################
 
-$BryanEthernetURL = ""
+$BryanEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Custom/Bryan's%20Desktop%20-%20Intel%20Core%20Ultra%209/Ethernet/ASUS%20ROG%20Z890-A%20STRIX-EthernetDrivers.zip"
 
 ################################################
 # Bryan ASUS Z890-A STRIX WiFi Drivers URL
