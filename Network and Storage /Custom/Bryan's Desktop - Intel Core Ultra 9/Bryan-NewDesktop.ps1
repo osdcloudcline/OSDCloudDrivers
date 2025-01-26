45c74eb9bf73a46cefc00a676b853a7b736d39f3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-26 09:14:49 -0500

    Update Bryan-NewDesktop.ps1

--- Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
+++ Network and Storage /Custom/Bryan's Desktop - Intel Core Ultra 9/Bryan-NewDesktop.ps1
@@ -8,23 +8,43 @@ Import-Module -Name OSD -Force
 Import-Module -Name 7Zip4Powershell -Force
 
 ################################################
-# Bryan ASUS Z890-A STRIX Ethernet Drivers  Path
+# Bryan ASUS Z890-A STRIX Ethernet Drivers URL
 ################################################
 
+$BryanEthernetURL = ""
+
+################################################
+# Bryan ASUS Z890-A STRIX WiFi Drivers URL
+################################################
+
+$BryanWiFiURL = ""
+
+################################################
+# Bryan ASUS Z890-A STRIX Bluetooth Drivers URL
+################################################
+
+$BryanBluetoothURL = ""
+
+################################################
+# Bryan ASUS Z890-A STRIX Storage Drivers URL
+################################################
+
+$BryanStorageURL = ""
 
 
 ################################################
-# Bryan ASUS Z890-A STRIX WiFi Drivers  Path
+# Bryan ASUS Z890-A STRIX Download Locations
 ################################################
 
-
-
-################################################
-# Bryan ASUS Z890-A STRIX Bluetooth Drivers  Path
-################################################
-
+$ZIPDownload = ""
 
 
 ################################################
-# Bryan ASUS Z890-A STRIX Storage Drivers  Path
+# Bryan ASUS Z890-A STRIX Extraction Locations
 ################################################
+
+$Ethernet = ""
+$WiFi = ""
+$Bluetooth = ""
+$Storage = ""
+
