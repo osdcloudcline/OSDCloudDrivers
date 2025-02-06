60c601eecc60aac4a28f388fdcbae3a71aaf5886
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:19:52 -0500

    Update AQUA.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/AQUA Motherboards/AQUA.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/AQUA Motherboards/AQUA.ps1
@@ -1,1 +1,32 @@
+$LGA1700AQUA = "C:\Logs\OSDCloud\ASRock\LGA1700\AQUA.log"
+Start-Transcript -Path $LGA1700AQUA
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
