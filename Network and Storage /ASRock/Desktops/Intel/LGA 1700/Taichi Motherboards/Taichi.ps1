28cad0fc697566c3085f33890aa2fb338bf8b5ad
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:28:36 -0500

    Update Taichi.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Taichi.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Taichi.ps1
@@ -1,1 +1,32 @@
+$LGA1700Taichi = "C:\Logs\OSDCloud\ASRock\LGA1700\Taichi.log"
+Start-Transcript -Path $LGA1700Taichi
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
