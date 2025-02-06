95f93381989861a588893c0c03728cfde3d80dd8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:27:36 -0500

    Update SteelLegend.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Steel Legend Motherboards/SteelLegend.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Steel Legend Motherboards/SteelLegend.ps1
@@ -1,1 +1,32 @@
+$LGA1700SteelLegend = "C:\Logs\OSDCloud\ASRock\LGA1700\SteelLegend.log"
+Start-Transcript -Path $LGA1700SteelLegend
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
