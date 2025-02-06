f2c5bca7a99823f81019d2509874710b9a520905
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:23:51 -0500

    Update PhantomGaming.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Phantom Gaming Motherboards/PhantomGaming.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Phantom Gaming Motherboards/PhantomGaming.ps1
@@ -1,1 +1,32 @@
+$LGA1700PhantomGaming = "C:\Logs\OSDCloud\ASRock\LGA1700\PhantomGaming.log"
+Start-Transcript -Path $LGA1700PhantomGaming
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
