3b43553a4394e4aa4817aaf2440e108d44fbbe6f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:22:53 -0500

    Update PRO.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/PRO Motherboards/PRO.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/PRO Motherboards/PRO.ps1
@@ -1,1 +1,32 @@
+$LGA1700PRO = "C:\Logs\OSDCloud\ASRock\LGA1700\PRO.log"
+Start-Transcript -Path $LGA1700PRO
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
