55954a1e80e564b54ea3e818df65917511858d3c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:45:46 -0500

    Update LGA1700-Storage.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Storage/LGA1700-Storage.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Storage/LGA1700-Storage.ps1
@@ -11,30 +11,20 @@ $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 
 $LGA1700StoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Storage"
 $StorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Storage/ASRock-LGA1700-IRST-StorageDrivers.zip"
-$StorageDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Storage"
+$StorageDownloadPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Storage\IRST"
 
 Import-Module -Name OSD -Force
 Import-Module -Name 7Zip4Powershell -Force
 
 
+Write-Verbose "Processing: ASRock LGA 1700 Storage Drivers..." -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $StorageURL -DestinationDirectory $StorageDownloadPath
+Expand-7zip -ArchiveFileName "$StorageDownloadPath\ASRock-LGA1700-IRST-StorageDrivers.zip" -TargetPath $LGA1700StoragePath -ErrorAction SilentlyContinue
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Write-Verbose "Completed: ASRock LGA 1700 Storage Drivers..." -Verbose
+Write-Host
 
 Stop-Transcript
