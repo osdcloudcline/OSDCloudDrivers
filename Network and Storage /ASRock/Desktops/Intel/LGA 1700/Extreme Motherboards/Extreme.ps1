1f99a931cbb6c0e5b657aed7f3e4c70e337c5d63
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:20:48 -0500

    Update Extreme.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Extreme Motherboards/Extreme.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Extreme Motherboards/Extreme.ps1
@@ -1,1 +1,32 @@
+$LGA1700Extreme = "C:\Logs\OSDCloud\ASRock\LGA1700\Extreme.log"
+Start-Transcript -Path $LGA1700Extreme
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
