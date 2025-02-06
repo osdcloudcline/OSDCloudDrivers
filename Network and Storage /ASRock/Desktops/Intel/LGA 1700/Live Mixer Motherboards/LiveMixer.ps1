7e31d5da82583c611dcb612d1302587c798b6a52
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:21:51 -0500

    Update LiveMixer.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Live Mixer Motherboards/LiveMixer.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Live Mixer Motherboards/LiveMixer.ps1
@@ -1,1 +1,32 @@
+$LGA1700LiveMixer = "C:\Logs\OSDCloud\ASRock\LGA1700\LiveMixer.log"
+Start-Transcript -Path $LGA1700LiveMixer
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
