5917f414ad4171b668fd2ca02f59b4a929778336
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-06 08:30:19 -0500

    Update IRSTStorage.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Storage/IRSTStorage.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Storage/IRSTStorage.ps1
@@ -1,1 +1,32 @@
+$IntelLGA1700Storage = "C:\Logs\OSDCloud\ASRock\LGA1700\IRST-StorageDrivers.log"
+Start-Transcript -Path $IntelLGA1700Storage
 
+Get-Date
+
+$GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Stop-Transcript
