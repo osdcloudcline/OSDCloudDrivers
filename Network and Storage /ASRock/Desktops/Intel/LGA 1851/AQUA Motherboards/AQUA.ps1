88045bc0f2e7e6236c95278b497f11aee7f91efc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-07 07:51:36 -0500

    Update AQUA.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1851/AQUA Motherboards/AQUA.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1851/AQUA Motherboards/AQUA.ps1
@@ -15,14 +15,11 @@ Import-Module -Name 7Zip4Powershell -Force
 
 $AQUABluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Bluetooth/ASRock-LGA1851-AQUA-BluetoothDrivers.zip"
 
-$AQUAEthernet1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers1.zip"
-$AQUAEthernet2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers2.zip"
-
-$AQUAEthernet3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers1.zip"
-$AQUAEthernet4URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers2.zip"
-
-$AQUAEthernet5URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers1.zip"
-$AQUAEthernet6URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers2.zip"
-
-$AQUAEthernet7URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/AQUA%20Motherboards/Ethernet/ASRock-LGA1700-AQUA-EthernetDrivers1.zip"
+$AQUAEthernet1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Aquatia-EthernetDrivers1.zip"
+$AQUAEthernet2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Killer-EthernetDrivers2.zip"
+$AQUAEthernet3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Killer-EthernetDrivers3.zip"
+$AQUAEthernet4URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Killer-EthernetDrivers4.zip"
+$AQUAEthernet5URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Killer-EthernetDrivers5.zip"
+$AQUAEthernet6URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Killer-EthernetDrivers6.zip"
+$AQUAEthernet7URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201851/AQUA%20Motherboards/Ethernet/ASRock-LGA1851-AQUA-Realtek-EthernetDrivers7.zip"
 
