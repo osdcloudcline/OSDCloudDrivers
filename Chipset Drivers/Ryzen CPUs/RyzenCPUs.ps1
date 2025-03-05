19b16a005bdf692b2c01cc628a6aa79b6b6da04c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-05 07:43:56 -0500

    Update RyzenCPUs.ps1

--- Chipset Drivers/Ryzen CPUs/RyzenCPUs.ps1
+++ Chipset Drivers/Ryzen CPUs/RyzenCPUs.ps1
@@ -5,7 +5,7 @@ Import-Module -Name OSD -Force
 
 $ChipsetSource1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Chipset%20Drivers/Ryzen%20CPUs/AMD-AutoDetect-version-24.12.1.zip.001"
 $ChipsetSource2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Chipset%20Drivers/Ryzen%20CPUs/AMD-AutoDetect-version-24.12.1.zip.002"
-$ChipsetSource3 = "https://drivers.amd.com/drivers/amd-software-adrenalin-edition-25.2.1-win10-win11-feb2025-rdna.exe"
+$ChipsetSource3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Chipset%20Drivers/Ryzen%20CPUs/AMD-AutoDetect-version-24.12.1.zip.003"
 $ChipsetSource4 = "https://download.amd.com/Desktop/amd-ryzen-master-2-14-1-3286.exe"
 
 $Destination = "C:\OSDCloud\drivers\Chipsets\RyzenCPUs"
