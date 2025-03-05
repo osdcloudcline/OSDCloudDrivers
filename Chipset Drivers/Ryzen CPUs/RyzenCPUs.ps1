9dab2910ac7b1abdcafc2471d53cb41c095eddd2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-05 07:42:47 -0500

    Update RyzenCPUs.ps1

--- Chipset Drivers/Ryzen CPUs/RyzenCPUs.ps1
+++ Chipset Drivers/Ryzen CPUs/RyzenCPUs.ps1
@@ -1,15 +1,19 @@
-$Chipset1 = "AMD Ryzen CPU Chipset version 24.10.1"
+$Chipset1 = "AMD Ryzen CPU Chipset version 24.12.1"
 $Chipset2 = "AMD Ryzen Master Software Version 2.14.1.3286"
 
 Import-Module -Name OSD -Force
 
 $ChipsetSource1 = "https://drivers.amd.com/drivers/amd-software-adrenalin-edition-25.2.1-win10-win11-feb2025-rdna.exe"
-$ChipsetSource2 = "https://download.amd.com/Desktop/amd-ryzen-master-2-14-1-3286.exe"
+$ChipsetSource2 = "https://drivers.amd.com/drivers/amd-software-adrenalin-edition-25.2.1-win10-win11-feb2025-rdna.exe"
+$ChipsetSource3 = "https://drivers.amd.com/drivers/amd-software-adrenalin-edition-25.2.1-win10-win11-feb2025-rdna.exe"
+$ChipsetSource4 = "https://download.amd.com/Desktop/amd-ryzen-master-2-14-1-3286.exe"
 
 $Destination = "C:\OSDCloud\drivers\Chipsets\RyzenCPUs"
 
 Write-Verbose "Processing: $Chipset1..." -Verbose
 Save-WebFile -SourceUrl $ChipsetSource1 -DestinationDirectory $Destination
+Save-WebFile -SourceUrl $ChipsetSource2 -DestinationDirectory $Destination
+Save-WebFile -SourceUrl $ChipsetSource3 -DestinationDirectory $Destination
 
 Write-Verbose "Processing: $Chipset2..." -Verbose
 Save-WebFile -SourceUrl $ChipsetSource2 -DestinationDirectory $Destination
