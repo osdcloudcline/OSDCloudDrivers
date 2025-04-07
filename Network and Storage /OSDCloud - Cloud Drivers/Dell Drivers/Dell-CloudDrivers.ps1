bc7711d6a82796a7de0567e2ad1c1b7702fc21c4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-07 13:35:47 -0400

    Update Dell-CloudDrivers.ps1

--- Network and Storage /OSDCloud - Cloud Drivers/Dell Drivers/Dell-CloudDrivers.ps1
+++ Network and Storage /OSDCloud - Cloud Drivers/Dell Drivers/Dell-CloudDrivers.ps1
@@ -20,24 +20,28 @@ $DellCloudDriversDestination = "C:\Drivers\Cloud Drivers\Dell"
 Get-Date
 
 Write-Verbose "Processing: OSDCloud Cloud Drivers - Downloading Dell Network Drivers..." -Verbose
+Write-Host
 
 Save-WebFile -SourceUrl $DellCloudDriversURL1 -DestinationDirectory "$DellCloudDriversDestination\Network\Windows10"
 Save-WebFile -SourceUrl $DellCloudDriversURL2 -DestinationDirectory "$DellCloudDriversDestination\Network\Windows10"
 Save-WebFile -SourceUrl $DellCloudDriversURL3 -DestinationDirectory "$DellCloudDriversDestination\Network\Windows11"
 
 Write-Verbose "Processing: OSDCloud Cloud Drivers - Downloading Dell Windows PE Drivers..." -Verbose
+Write-Host
 
 Save-WebFile -SourceUrl $DellCloudDriversURL4 -DestinationDirectory "$DellCloudDriversDestination\Windows PE"
 
 Write-Verbose "Processing: OSDCloud Cloud Drivers - Downloading Dell Storage Drivers..." -Verbose
+Write-Host
 
 Save-WebFile -SourceUrl $DellCloudDriversURL5 -DestinationDirectory "$DellCloudDriversDestination\Storage\Other"
 Save-WebFile -SourceUrl $DellCloudDriversURL6 -DestinationDirectory "$DellCloudDriversDestination\Storage\Windows10"
 Save-WebFile -SourceUrl $DellCloudDriversURL7 -DestinationDirectory "$DellCloudDriversDestination\Storage\Windows11"
 
+Write-Host "Comppleted: Dell Network, Storage and Windows PE driver downloads..." -ForegroundColor Cyan
 
 Write-Verbose "Processing: OSDCloud Cloud Drivers - Dell ZIP Files..." -Verbose
-
+Write-Host
 
 Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Network\Windows10\Dell-CloudDrivers-Network-Windows10-18362.zip" -TargetPath "$DellCloudDriversDestination\Network\Windows10\18362" -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Network\Windows10\Dell-CloudDrivers-Network-Windows10-22000.zip" -TargetPath "$DellCloudDriversDestination\Network\Windows10\22000" -ErrorAction SilentlyContinue 
@@ -47,5 +51,7 @@ Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Windows PE\Dell-Cloud
 Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Storage\Other\Dell-CloudDrivers-Storage-Other.zip" -TargetPath "$DellCloudDriversDestination\Storage\Other\Extract" -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Storage\Windows10\Dell-CloudDrivers-Storage-Windows10.zip" -TargetPath "$DellCloudDriversDestination\Storage\Windows10\Extract" -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$DellCloudDriversDestination\Storage\Windows11\Dell-CloudDrivers-Storage-Windows11.zip" -TargetPath "$DellCloudDriversDestination\Storage\Windows11\Extract" -ErrorAction SilentlyContinue 
+
+Write-Host "Comppleted: ZIP File extraction..." -ForegroundColor Cyan
 
 Stop-Transcript
