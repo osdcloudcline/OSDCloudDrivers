98a058f8c79b72f4ad2a9ea456abed8fac31a021
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-07 22:13:25 -0400

    Create WLAN-CloudDrivers.ps1

---
+++ Network and Storage /OSDCloud - Cloud Drivers/WLAN Drivers/WLAN-CloudDrivers.ps1
@@ -0,0 +1,31 @@
+$OSDCloudCloudDriversWLANLog = "C:\Logs\OSDCloud\Drivers\Cloud Drivers\WLAN.log"
+
+Start-Transcript -Path $OSDCloudCloudDriversWLANLog
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4Powershell -Force
+
+
+$WLANCloudDriversURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/OSDCloud%20-%20Cloud%20Drivers/WLAN%20Drivers/WLAN-CloudDrivers.zip"
+
+$WLANCloudDriversDestination = "C:\Drivers\Cloud Drivers\WLAN"
+
+Get-Date
+
+Write-Verbose "Processing: OSDCloud Cloud Drivers - Downloading WLAN Drivers..." -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $WLANCloudDriversURL1 -DestinationDirectory "$WLANCloudDriversDestination"
+
+
+Write-Host "Completed: WLAN driver downloads..." -ForegroundColor Cyan
+
+Write-Verbose "Processing: OSDCloud Cloud Drivers - WLAN ZIP Files..." -Verbose
+Write-Host
+
+Expand-7Zip -ArchiveFileName "$WLANCloudDriversDestination\WLAN-CloudDrivers1.zip" -TargetPath "$WLANCloudDriversDestination\Extract" -ErrorAction SilentlyContinue 
+
+
+Write-Host "Completed: ZIP File extraction..." -ForegroundColor Cyan
+
+Stop-Transcript
