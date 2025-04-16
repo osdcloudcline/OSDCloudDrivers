c6cfced98cbf64d3ff2b3263b2efbbf351c81efe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 08:11:23 -0400

    Update Fatal1tyGaming.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM4/Fatal1ty Gaming Motherboards/Fatal1tyGaming.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM4/Fatal1ty Gaming Motherboards/Fatal1tyGaming.ps1
@@ -18,8 +18,6 @@ $Fatal1tyGamingEthernetURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/
 $Fatal1tyGamingWiFiURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Ethernet%20and%20WiFi/ASRock-Fatal1tyGaming-Intel-WLANDrivers.zip"
 $Fatal1tyGamingWiFiURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Ethernet%20and%20WiFi/ASRock-Fatal1tyGaming-Intel-WLANDrivers2.zip"
 
-$Fatal1tyGamingBluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Ethernet%20and%20WiFi/ASRock-Fatal1tyGaming-Intel-BluetoothDrivers.zip"
-
 $Fatal1tyGamingStorage1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Storage/ASRock-Fatal1tyGaming-NVMe-StorageDrivers.zip"
 $Fatal1tyGamingStorage2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Storage/ASRock-Fatal1tyGaming-SATA-StorageDrivers.zip"
 
@@ -27,7 +25,7 @@ $Fatal1tyGamingStorage2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/r
 #             AM4 Ethernet, WiFi and Storage Driver Download Destination
 ####################################################################################
 
-$Fatal1tyGamingDestination = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming"
+$Fatal1tyGamingDestination = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming"
 
 ####################################################################################
 #             AM4 Ethernet, WiFi and Storage Driver Extraction Destination
@@ -38,8 +36,6 @@ $Fatal1tyGamingEthernet2 = "$Fatal1tyGamingDestination\Ethernet2"
 
 $Fatal1tyGamingWiFi1 = "$Fatal1tyGamingDestination\WiFi1"
 $Fatal1tyGamingWiFi2 = "$Fatal1tyGamingDestination\WiFi2"
-
-$Fatal1tyGamingBluetooth = "$Fatal1tyGamingDestination\Bluetooth"
 
 $Fatal1tyGamingStorage1 = "$Fatal1tyGamingDestination\Storage1"
 $Fatal1tyGamingStorage2 = "$Fatal1tyGamingDestination\Storage2"
@@ -59,13 +55,6 @@ Write-Host
 Save-WebFile -SourceUrl $Fatal1tyGamingWiFiURL1 -DestinationDirectory $Fatal1tyGamingDestination
 Save-WebFile -SourceUrl $Fatal1tyGamingWiFiURL2 -DestinationDirectory $Fatal1tyGamingDestination
 
-
-Write-Verbose "Acquiring ASRock AM4 Fatal1ty Gaming Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $Fatal1tyGamingBluetoothURL -DestinationDirectory $Fatal1tyGamingDestination
-
-
 Write-Verbose "Acquiring ASRock AM4 Fatal1ty Gaming Motherboard Storage Drivers from $GHURL" -Verbose
 Write-Host
 
@@ -83,8 +72,6 @@ Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-R
 
 Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-Intel-WLANDrivers.zip" -TargetPath $Fatal1tyGamingWiFi1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-Intel-WLANDrivers2.zip" -TargetPath $Fatal1tyGamingWiFi2  -ErrorAction SilentlyContinue 
-
-Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-Intel-BluetoothDrivers.zip" -TargetPath $Fatal1tyGamingBluetooth  -ErrorAction SilentlyContinue 
 
 Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-NVMe-StorageDrivers.zip" -TargetPath $Fatal1tyGamingStorage1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$Fatal1tyGamingDestination\ASRock-Fatal1tyGaming-SATA-StorageDrivers.zip" -TargetPath $Fatal1tyGamingStorage2  -ErrorAction SilentlyContinue 
