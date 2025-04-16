43f7242434be52b2ac4cfb58cd19e06994134088
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 07:14:09 -0400

    Update ESXI-Drivers.ps1

--- Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
+++ Network and Storage /VMWare ESXI/ESXI-Drivers.ps1
@@ -14,9 +14,9 @@ Import-Module -Name 7Zip4Powershell -Force
 # ESXI Network Drivers  Path
 ##########################
 
-$ESXIPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Network"
+$ESXIPath1 = "C:\Drivers\Virtualization\ESXI\Network"
 $ESXINetworkDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
-$ESXIDownloadPath1 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Network"
+$ESXIDownloadPath1 = "C:\Drivers\Virtualization\ESXI\Download\Network"
 
 Import-Module -Name OSD -Force
 Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -Verbose
@@ -43,9 +43,9 @@ Get-Date
 # ESXI Storage Drivers Path
 ##########################
 
-$ESXIPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Storage"
+$ESXIPath2 = "C:\Drivers\Virtualization\ESXI\Storage"
 $ESXIStorageDriversURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXIDrivers.zip"
-$ESXIDownloadPath2 = "C:\OSDCloud\Drivers\Virtualization\ESXI\Download\Storage"
+$ESXIDownloadPath2 = "C:\Drivers\Virtualization\ESXI\Download\Storage"
 
 Import-Module -Name OSD -Force
 
