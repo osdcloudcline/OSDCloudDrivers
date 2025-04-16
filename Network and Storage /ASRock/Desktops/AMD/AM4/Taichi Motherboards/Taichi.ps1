0bf7c896ecb2233cd1c10b7a2aafee1aa2ecab80
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 11:25:13 -0400

    Update Taichi.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM4/Taichi Motherboards/Taichi.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM4/Taichi Motherboards/Taichi.ps1
@@ -14,8 +14,6 @@ Import-Module -Name 7Zip4Powershell -Force
 
 $TaichiEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Taichi%20Motherboards/Ethernet%20and%20WiFi/ASRock-Taichi-Intel-LANDrivers.zip"
 
-$TaichiBluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Taichi%20Motherboards/Ethernet%20and%20WiFi/ASRock-Taichi-BluetoothDrivers.zip"
-
 $TaichiWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Taichi%20Motherboards/Ethernet%20and%20WiFi/ASRock-Taichi-Intel-WLANDrivers.zip"
 
 $TaichiStorageURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Taichi%20Motherboards/Storage/ASRock-Taichi-NVMe-StorageDrivers.zip"
@@ -25,15 +23,13 @@ $TaichiStorageURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/
 #             AM4 Ethernet, WiFi and Storage Driver Download Destination
 ####################################################################################
 
-$TaichiDestination = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Taichi"
+$TaichiDestination = "C:\Drivers\Motherboards\ASRock\AM4\Taichi"
 
 ####################################################################################
 #             AM4 Ethernet, WiFi and Storage Driver Extraction Destination
 ####################################################################################
 
 $TaichiEthernet = "$TaichiDestination\Ethernet"
-
-$TaichiBluetooth = "$TaichiDestination\Bluetooth"
 
 $TaichiWiFi = "$TaichiDestination\WiFi"
 
@@ -46,11 +42,6 @@ Write-Verbose "Acquiring ASRock AM4 Taichi Motherboard Ethernet Drivers from $GH
 Write-Host
 
 Save-WebFile -SourceUrl $TaichiEthernetURL -DestinationDirectory $TaichiDestination
-
-Write-Verbose "Acquiring ASRock AM4 Taichi Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $TaichiBluetoothURL -DestinationDirectory $TaichiDestination
 
 Write-Host
 Write-Verbose "Acquiring ASRock AM4 Taichi Motherboard WiFi Drivers from $GHURL" -Verbose
@@ -73,7 +64,6 @@ Write-Verbose "Processing: ZIP File extraction" -Verbose
 Write-Host
 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-Taichi-Intel-LANDrivers.zip" -TargetPath $TaichiEthernet  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-Taichi-BluetoothDrivers.zip" -TargetPath $TaichiBluetooth  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestinationn\ASRock-Taichi-Intel-WLANDrivers.zip" -TargetPath $TaichiWiFi  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-Taichi-NVMe-StorageDrivers.zip" -TargetPath $TaichiStorage1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-Taichi-SATA-StorageDrivers.zip" -TargetPath $TaichiStorage2  -ErrorAction SilentlyContinue 
