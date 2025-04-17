6b508f0250bb883d02d09f4eef30f672da8510be
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 18:01:02 -0400

    Update Taichi.ps1

--- Network and Storage /ASRock/Desktops/AMD/AM5/Taichi Motherboards/Taichi.ps1
+++ Network and Storage /ASRock/Desktops/AMD/AM5/Taichi Motherboards/Taichi.ps1
@@ -12,8 +12,7 @@ Import-Module -Name 7Zip4Powershell -Force
 #                   AM5 Ethernet, WiFi and Storage Driver URLs
 ####################################################################################
 
-$TaichiBluetoothURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM5/Taichi%20Motherboards/Network%20and%20WiFi/ASRock-AM5-Taichi-Intel-BluetoothDrivers.zip"
-$TaichiBluetoothURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM5/Taichi%20Motherboards/Network%20and%20WiFi/ASRock-AM5-Taichi-MediaTek-BluetoothDrivers.zip"
+
 $TaichiEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM5/Taichi%20Motherboards/Network%20and%20WiFi/ASRock-AM5-Taichi-Realtek-EthernetDrivers.zip"
 $TaichiWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM5/Taichi%20Motherboards/Network%20and%20WiFi/ASRock-AM5-Taichi-MediaTek-WiFiDrivers.zip"
 
@@ -24,17 +23,15 @@ $TaichiStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/h
 #             AM5 Ethernet, WiFi and Storage Driver Download Destination
 ####################################################################################
 
-$TaichiDestination = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi"
+$TaichiDestination = "C:\Drivers\Motherboards\ASRock\AM5\Taichi"
 
 ####################################################################################
 #             AM5 Ethernet,Bluetooth  WiFi and Storage Driver Extraction Destination
 ####################################################################################
 
 $TaichiEthernet = "$TaichiDestination\Ethernet"
-$TaichiBluetooth1 = "$TaichiDestination\Bluetooth1"
-$TaichiBluetooth2 = "$TaichiDestination\Bluetooth2"
-$TaichiWiFi = "$TaichiDestination\WiFi"
 
+$TaichiWiFi = "$TaichiDestination\WiFi"
 
 $TaichiStorage = "$TaichiDestination\Storage"
 
@@ -45,12 +42,6 @@ Write-Verbose "Acquiring ASRock AM5 Taichi Motherboard Ethernet Drivers from $GH
 Write-Host
 
 Save-WebFile -SourceUrl $TaichiEthernetURL -DestinationDirectory $TaichiDestination
-
-Write-Verbose "Acquiring ASRock AM5 Taichi Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $TaichiBluetoothURL1 -DestinationDirectory $TaichiDestination
-Save-WebFile -SourceUrl $TaichiBluetoothURL2 -DestinationDirectory $TaichiDestination
 
 Write-Verbose "Acquiring ASRock AM5 Taichi Motherboard WiFi Drivers from $GHURL" -Verbose
 Write-Host
@@ -68,8 +59,6 @@ Write-Host
 Write-Verbose "Processing: ZIP File extraction" -Verbose
 Write-Host
 
-Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-AM5-Taichi-Intel-BluetoothDrivers.zip" -TargetPath $TaichiBluetooth1  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-AM5-Taichi-MediaTek-BluetoothDrivers.zip" -TargetPath $TaichiBluetooth2  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-AM5-Taichi-Realtek-EthernetDrivers.zip" -TargetPath $TaichiEthernet  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-AM5-Taichi-MediaTek-WiFiDrivers.zip" -TargetPath $TaichiWiFi  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-AM5-Taichi-RAID-NVMe-StorageDrivers.zip" -TargetPath $TaichiStorage  -ErrorAction SilentlyContinue 
