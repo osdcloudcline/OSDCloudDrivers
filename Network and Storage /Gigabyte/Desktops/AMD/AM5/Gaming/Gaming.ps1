1831c0ac0c457f4fc54613400a95b4ad2c8f7a97
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-18 08:06:58 -0400

    Update Gaming.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM5/Gaming/Gaming.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM5/Gaming/Gaming.ps1
@@ -6,31 +6,6 @@ Get-Date
 $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 
 ####################################################################################
-#                   AM5 Bluetooth Driver URLs
-####################################################################################
-
-# Gaming A620 - Bluetooth
-
-$GamingBluetoothURL1 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/A620/Bluetooth/Gigabyte-AM5-Gaming-A620-BluetoothDrivers.zip"
-
-# Gaming B650 - Bluetooth
-
-$GamingBluetoothURL2 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/B650/Bluetooth/Gigabyte-AM5-Gaming-B650-BluetoothDrivers1.zip"
-$GamingBluetoothURL3 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/B650/Bluetooth/Gigabyte-AM5-Gaming-B650-BluetoothDrivers2.zip"
-
-# Gaming X670 - Bluetooth
-
-$GamingBluetoothURL4 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/X670/Bluetooth/Gigabyte-AM5-Gaming-X670-BluetoothDrivers1.zip"
-$GamingBluetoothURL5 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/X670/Bluetooth/Gigabyte-AM5-Gaming-X670-BluetoothDrivers2.zip"
-
-# Gaming X870 - Bluetooth
-
-$GamingBluetoothURL6 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/X870/Bluetooth/Gigabyte-AM5-Gaming-X870-BluetoothDrivers1.zip"
-$GamingBluetoothURL7 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/X870/Bluetooth/Gigabyte-AM5-Gaming-X870-BluetoothDrivers2.zip"
-$GamingBluetoothURL8 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/X870/Bluetooth/Gigabyte-AM5-Gaming-X870-BluetoothDrivers3.zip"
-
-
-####################################################################################
 #                   AM5 Ethernet Driver URLs
 ####################################################################################
 
@@ -111,35 +86,15 @@ $GamingWiFiURL12 = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/he
 #        Gaming Ethernet, Bluetooth, Storage and WiFi Destination Folders
 ###########################################################################################
 
-$GamingDestination =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\Gaming"
-$GamingDestinationA620 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\A620"
-$GamingDestinationB650 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\B650"
-$GamingDestinationX670 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\X670"
-$GamingDestinationX870 =  "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\X870"
+$GamingDestination =  "C:\Drivers\Motherboards\Gigabyte\AM5\Gaming"
+$GamingDestinationA620 =  "C:\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\A620"
+$GamingDestinationB650 =  "C:\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\B650"
+$GamingDestinationX670 =  "C:\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\X670"
+$GamingDestinationX870 =  "C:\Drivers\Motherboards\Gigabyte\AM5\Gaming\ZIP\X870"
 
 ###########################################################################################
 #        Gaming Ethernet, Bluetooth, Storage and WiFi Extraction Folders
 ###########################################################################################
-
-# Gaming A620 - Bluetooth
-
-$GamingBluetooth1 = "$GamingDestination\A620\Bluetooth"
-
-# Gaming B650 - Bluetooth
-
-$GamingBluetooth2 = "$GamingDestination\B650\Bluetooth1"
-$GamingBluetooth3 = "$GamingDestination\B650\Bluetooth2"
-
-# Gaming X670 - Bluetooth
-
-$GamingBluetooth4 = "$GamingDestination\X670\Bluetooth1"
-$GamingBluetooth5 = "$GamingDestination\X670\Bluetooth2"
-
-# Gaming X670 - Bluetooth
-
-$GamingBluetooth6 = "$GamingDestination\X870\Bluetooth1"
-$GamingBluetooth7 = "$GamingDestination\X870\Bluetooth2"
-$GamingBluetooth8 = "$GamingDestination\X870\Bluetooth3"
 
 # Gaming A620 - Ethernet
 
@@ -177,29 +132,29 @@ $GamingStorage3 = "$GamingDestination\X670\Storage\RAID\NVMe"
 
 $GamingStorage4 = "$GamingDestination\X870\Storage\RAID\NVMe"
 
-# Gaming A620 - WiFi
+# Gaming A620 - WLAN
 
-$GamingWiFi1 = "$GamingDestination\A620\WiFi"
+$GamingWLAN1 = "$GamingDestination\A620\WLAN"
 
-# Gaming B650 - WiFi
+# Gaming B650 - WLAN
 
-$GamingWiFi2 = "$GamingDestination\B650\WiFi1"
-$GamingWiFi3 = "$GamingDestination\B650\WiFi2"
-$GamingWiFi4 = "$GamingDestination\B650\WiFi3"
+$GamingWLAN2 = "$GamingDestination\B650\WLAN1"
+$GamingWLAN3 = "$GamingDestination\B650\WLAN2"
+$GamingWLAN4 = "$GamingDestination\B650\WLAN3"
 
-# Gaming X670 - WiFi
+# Gaming X670 - WLAN
 
-$GamingWiFi5 = "$GamingDestination\X670\WiFi1"
-$GamingWiFi6 = "$GamingDestination\X670\WiFi2"
+$GamingWLAN5 = "$GamingDestination\X670\WLAN1"
+$GamingWLAN6 = "$GamingDestination\X670\WLAN2"
 
-# Gaming X870 - WiFi
+# Gaming X870 - WLAN
 
-$GamingWiFi7 = "$GamingDestination\X870\WiFi1"
-$GamingWiFi8 = "$GamingDestination\X870\WiFi2"
-$GamingWiFi9 = "$GamingDestination\X870\WiFi3"
-$GamingWiFi10 = "$GamingDestination\X870\WiFi4"
-$GamingWiFi11 = "$GamingDestination\X870\WiFi5"
-$GamingWiFi12 = "$GamingDestination\X870\WiFi6"
+$GamingWLAN7 = "$GamingDestination\X870\WLAN1"
+$GamingWLAN8 = "$GamingDestination\X870\WLAN2"
+$GamingWLAN9 = "$GamingDestination\X870\WLAN3"
+$GamingWLAN10 = "$GamingDestination\X870\WLAN4"
+$GamingWLAN11 = "$GamingDestination\X870\WLAN5"
+$GamingWLAN12 = "$GamingDestination\X870\WLAN6"
 
 
 Write-Host
@@ -248,54 +203,6 @@ Write-Host
 
 Write-Host
 Write-Verbose "Completed: Download of ALL Gigabyte Gaming AM5 motherboard Ethernet Drivers..." -Verbose
-
-
-Write-Host
-Write-Verbose "Processing: Acquiring Gigabyte Gaming AM5 A620 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $GamingBluetoothURL1 -DestinationDirectory $GamingDestinationA620
-
-Write-Host
-Write-Verbose "Completed: Download of Gigabyte Gaming AM5 A620 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Write-Host
-Write-Verbose "Processing: Acquiring Gigabyte Gaming AM5 B650 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $GamingBluetoothURL2 -DestinationDirectory $GamingDestinationB650
-Save-WebFile -SourceUrl $GamingBluetoothURL3 -DestinationDirectory $GamingDestinationB650
-
-Write-Host
-Write-Verbose "Completed: Download of Gigabyte Gaming AM5 B650 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Write-Host
-Write-Verbose "Processing: Acquiring Gigabyte Gaming AM5 X670 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $GamingBluetoothURL4 -DestinationDirectory $GamingDestinationX670
-Save-WebFile -SourceUrl $GamingBluetoothURL5 -DestinationDirectory $GamingDestinationX670
-
-Write-Host
-Write-Verbose "Completed: Download of Gigabyte Gaming AM5 X670 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Write-Host
-Write-Verbose "Processing: Acquiring Gigabyte Gaming AM5 X870 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $GamingBluetoothURL6 -DestinationDirectory $GamingDestinationX870
-Save-WebFile -SourceUrl $GamingBluetoothURL7 -DestinationDirectory $GamingDestinationX870
-Save-WebFile -SourceUrl $GamingBluetoothURL8 -DestinationDirectory $GamingDestinationX870
-
-Write-Host
-Write-Verbose "Completed: Download of Gigabyte Gaming AM5 X870 Bluetooth Drivers..." -Verbose
-Write-Host
-
-Write-Host
-Write-Verbose "Completed: Download of ALL Gigabyte Gaming AM5 motherboard Bluetooth Drivers..." -Verbose
 
 Write-Host
 Write-Verbose "Processing: Acquiring Gigabyte Gaming AM5 Storage Drivers..." -Verbose
@@ -388,23 +295,6 @@ Expand-7zip -ArchiveFileName "$GamingDestinationX870\Gigabyte-AM5-Gaming-X870-NV
 
 Write-Host
 Write-Verbose "Completed: Extraction of ALL Gigabyte Gaming AM5 Storage Drivers..." -Verbose
-Write-Host
-
-Write-Host
-Write-Verbose "Processing: Extracting Bluetooth drivers..." -Verbose
-Write-Host
-
-Expand-7zip -ArchiveFileName "$GamingDestinationA620\Gigabyte-AM5-Gaming-A620-BluetoothDrivers.zip" -TargetPath $GamingBluetooth1 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationB650\Gigabyte-AM5-Gaming-B650-BluetoothDrivers1.zip" -TargetPath $GamingBluetooth2 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationB650\Gigabyte-AM5-Gaming-B650-BluetoothDrivers2.zip" -TargetPath $GamingBluetooth3 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationX670\Gigabyte-AM5-Gaming-X670-BluetoothDrivers1.zip" -TargetPath $GamingBluetooth4 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationX670\Gigabyte-AM5-Gaming-X670-BluetoothDrivers2.zip" -TargetPath $GamingBluetooth5 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationX870\Gigabyte-AM5-Gaming-X870-BluetoothDrivers1.zip" -TargetPath $GamingBluetooth6 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationX870\Gigabyte-AM5-Gaming-X870-BluetoothDrivers2.zip" -TargetPath $GamingBluetooth7 -ErrorAction SilentlyContinue
-Expand-7zip -ArchiveFileName "$GamingDestinationX870\Gigabyte-AM5-Gaming-X870-BluetoothDrivers3.zip" -TargetPath $GamingBluetooth8 -ErrorAction SilentlyContinue
-
-Write-Host
-Write-Verbose "Completed: Extraction of ALL Gigabyte Gaming AM5 Bluetooth Drivers..." -Verbose
 Write-Host
 
 Write-Host
