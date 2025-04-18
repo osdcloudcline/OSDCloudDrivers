451dab1f7e364b0b0a93ee18bdc3c589e93cdeb9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 22:49:49 -0400

    Update CSM.ps1

--- Network and Storage /ASUS/Desktops/AMD/AM 5/CSM Motherboards/CSM.ps1
+++ Network and Storage /ASUS/Desktops/AMD/AM 5/CSM Motherboards/CSM.ps1
@@ -12,11 +12,6 @@ Import-Module -Name 7Zip4Powershell -Force
 #                   AM5 Ethernet, WiFi and Storage Driver URLs
 ####################################################################################
 
-$CSMBluetooth1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Bluetooth/ASUS-AM5-CSM-BluetoothDrivers1.zip"
-$CSMBluetooth2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Bluetooth/ASUS-AM5-CSM-BluetoothDrivers2.zip"
-$CSMBluetooth3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Bluetooth/ASUS-AM5-CSM-BluetoothDrivers3.zip"
-$CSMBluetooth4URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Bluetooth/ASUS-AM5-CSM-BluetoothDrivers4.zip"
-
 $CSMEthernet1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Ethernet/ASUS-AM5-CSM-EthernetDrivers1.zip"
 $CSMEthernet2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Ethernet/ASUS-AM5-CSM-EthernetDrivers2.zip"
 $CSMEthernet3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%205/CSM%20Motherboards/Ethernet/ASUS-AM5-CSM-EthernetDrivers3.zip"
@@ -39,16 +34,11 @@ $CSMWLAN4URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/
 #             AM5 Ethernet, WiFi and Storage Driver Download Destination
 ####################################################################################
 
-$CSMDestination = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM5\CSM"
+$CSMDestination = "C:\Drivers\Motherboards\ASUS\AM5\CSM"
 
 ####################################################################################
 #             AM5 Ethernet, WiFi and Storage Driver Extraction Destination
 ####################################################################################
-
-$CSMBluetooth1 = "$CSMDestination\Bluetooth1"
-$CSMBluetooth2 = "$CSMDestination\Bluetooth2"
-$CSMBluetooth3 = "$CSMDestination\Bluetooth3"
-$CSMBluetooth4 = "$CSMDestination\Bluetooth4"
 
 $CSMEthernet1 = "$CSMDestination\Ethernet1"
 $CSMEthernet2 = "$CSMDestination\Ethernet2"
@@ -68,15 +58,6 @@ $CSMWLAN2 = "$CSMDestination\WLAN2"
 $CSMWLAN3 = "$CSMDestination\WLAN3"
 $CSMWLAN4 = "$CSMDestination\WLAN4"
 
-Write-Verbose "Acquiring ASUS AM5 CSM Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $CSMBluetooth1URL -DestinationDirectory $CSMDestination
-Save-WebFile -SourceUrl $CSMBluetooth2URL -DestinationDirectory $CSMDestination
-Save-WebFile -SourceUrl $CSMBluetooth3URL -DestinationDirectory $CSMDestination
-Save-WebFile -SourceUrl $CSMBluetooth4URL -DestinationDirectory $CSMDestination
-
-
 Write-Verbose "Acquiring ASUS AM5 CSM Motherboard Ethernet Drivers from $GHURL" -Verbose
 Write-Host
 
@@ -85,7 +66,6 @@ Save-WebFile -SourceUrl $CSMEthernet2URL -DestinationDirectory $CSMDestination
 Save-WebFile -SourceUrl $CSMEthernet3URL -DestinationDirectory $CSMDestination
 Save-WebFile -SourceUrl $CSMEthernet4URL -DestinationDirectory $CSMDestination
 Save-WebFile -SourceUrl $CSMEthernet5URL -DestinationDirectory $CSMDestination
-
 Save-WebFile -SourceUrl $CSMEthernet6URL -DestinationDirectory $CSMDestination
 Save-WebFile -SourceUrl $CSMEthernet7URL -DestinationDirectory $CSMDestination
 Save-WebFile -SourceUrl $CSMEthernet8URL -DestinationDirectory $CSMDestination
@@ -111,11 +91,6 @@ Write-Host
 
 Write-Verbose "Processing: ZIP File extraction" -Verbose
 Write-Host
-
-Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-BluetoothDrivers1.zip" -TargetPath $CSMBluetooth1  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-BluetoothDrivers2.zip" -TargetPath $CSMBluetooth2  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-BluetoothDrivers3.zip" -TargetPath $CSMBluetooth3  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-BluetoothDrivers4.zip" -TargetPath $CSMBluetooth4  -ErrorAction SilentlyContinue 
 
 Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-EthernetDrivers1.zip" -TargetPath $CSMEthernet1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$CSMDestination\ASUS-AM5-CSM-EthernetDrivers2.zip" -TargetPath $CSMEthernet2  -ErrorAction SilentlyContinue 
