4cba0daf7fc83ca1791401623676bbbef60e2bb7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-18 07:50:59 -0400

    Update AI-TOP.ps1

--- Network and Storage /Gigabyte/Desktops/AMD/AM5/AI TOP/AI-TOP.ps1
+++ Network and Storage /Gigabyte/Desktops/AMD/AM5/AI TOP/AI-TOP.ps1
@@ -13,18 +13,15 @@ $GHURL = "https://github.com/osdcloudcline/OSDCloudDrivers"
 $AITOPEthernet1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/AI%20TOP/Ethernet/Gigabyte-AM5-AI%20TOP-EthernetDrivers-Windows10.zip"
 $AITOPEthernet2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/AI%20TOP/Ethernet/Gigabyte-AM5-AI%20TOP-EthernetDrivers-Windows11.zip"
 
-$AITOPBluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/AI%20TOP/Bluetooth/Gigabyte-AM5-AI%20TOP-BluetoothDrivers.zip"
-
 $AITOPWiFiURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/AI%20TOP/WiFi/Gigabyte-AM5-AI%20TOP-WLANDrivers.zip"
 
 $AITOPStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/AI%20TOP/Storage/Gigabyte-AM5-AI%20TOP-NVMe-RAID-StorageDrivers.zip"
-
 
 ####################################################################################
 #             AM5 Ethernet, WiFi and Storage Driver Download Destination
 ####################################################################################
 
-$AITOPDestination = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\AI-TOP"
+$AITOPDestination = "C:\Drivers\Motherboards\Gigabyte\AM5\AI-TOP"
 
 ####################################################################################
 #             AM5 Ethernet, WiFi and Storage Driver Extraction Destination
@@ -32,8 +29,6 @@ $AITOPDestination = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM5\AI-TOP"
 
 $AITOPEthernet1 = "$AITOPDestination\Ethernet1"
 $AITOPEthernet2 = "$AITOPDestination\Ethernet2"
-
-$AITOPBluetooth = "$AITOPDestination\Bluetooth"
 
 $AITOPWiFi = "$AITOPDestination\WiFi1"
 
@@ -48,23 +43,15 @@ Write-Host
 Save-WebFile -SourceUrl  $AITOPEthernet1URL -DestinationDirectory $AITOPDestination
 Save-WebFile -SourceUrl  $AITOPEthernet2URL -DestinationDirectory $AITOPDestination
 
-Write-Verbose "Acquiring Gigabyte AM5 AI TOP Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl  $AITOPBluetoothURL -DestinationDirectory $AITOPDestination
-
 Write-Verbose "Acquiring Gigabyte AM5 AI TOP Motherboard WiFi Drivers from $GHURL" -Verbose
 Write-Host
 
 Save-WebFile -SourceUrl  $AITOPWiFiURL -DestinationDirectory $AITOPDestination
 
-
-
 Write-Verbose "Acquiring Gigabyte AM5 AI TOP Motherboard Storage Drivers from $GHURL" -Verbose
 Write-Host
 
 Save-WebFile -SourceUrl $AITOPStorageURL -DestinationDirectory $AITOPDestination
-
 
 Write-Verbose "Gigabyte AM5 AI TOP Motherboard Drivers downloaded" -Verbose
 Write-Host
@@ -74,7 +61,6 @@ Write-Host
 
 Expand-7Zip -ArchiveFileName "$AITOPDestination\Gigabyte-AM5-AI%20TOP-EthernetDrivers-Windows10.zip" -TargetPath $AITOPEthernet1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$AITOPDestination\Gigabyte-AM5-AI%20TOP-EthernetDrivers-Windows11.zip" -TargetPath $AITOPEthernet2  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$AITOPDestination\Gigabyte-AM5-AI%20TOP-BluetoothDrivers.zip" -TargetPath $AITOPBluetooth  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$AITOPDestination\Gigabyte-AM5-AI%20TOP-WLANDrivers.zip" -TargetPath $AITOPWiFi  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$AITOPDestination\Gigabyte-AM5-AI%20TOP-NVMe-RAID-StorageDrivers.zip" -TargetPath $AITOPStorage  -ErrorAction SilentlyContinue 
 
