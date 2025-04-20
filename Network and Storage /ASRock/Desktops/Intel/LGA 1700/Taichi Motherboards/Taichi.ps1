67873838ca64b23bff2cadd5c2b9f10da8099965
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-19 23:41:53 -0400

    Update Taichi.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Taichi.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Taichi Motherboards/Taichi.ps1
@@ -12,9 +12,6 @@ Import-Module -Name 7Zip4Powershell -Force
 #          LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver URLs
 ####################################################################################
 
-
-$TaichiBluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Taichi%20Motherboards/Bluetooth/ASRock-LGA1700-Taichi-BluetoothDrivers.zip"
-
 $TaichiEthernetURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Taichi%20Motherboards/Ethernet/ASRock-LGA1700-Taichi-Intel-EthernetDrivers.zip"
 
 $TaichiStorageURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Taichi%20Motherboards/Storage/ASRock-LGA1700-Taichi-IRST-StorageDrivers.zip"
@@ -26,13 +23,11 @@ $TaichiWLANURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/head
 #    LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver Download Destination
 ####################################################################################
 
-$TaichiDestination = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Taichi"
+$TaichiDestination = "C:\Drivers\Motherboards\ASRock\LGA1700\Taichi"
 
 ####################################################################################
 #   LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver Extraction Destination
 ####################################################################################
-
-$TaichiBluetooth = "$TaichiDestination\Bluetooth"
 
 $TaichiEthernet = "$TaichiDestination\Ethernet"
 
@@ -40,17 +35,10 @@ $TaichiStorage = "$TaichiDestination\Storage"
 
 $TaichiWLAN = "$TaichiDestination\WLAN1"
 
-
-Write-Verbose "Processing: Downloading ASRock LGA 1700 Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $TaichiBluetoothURL -DestinationDirectory $TaichiDestination
-
 Write-Verbose "Processing: Downloading ASRock LGA 1700 Motherboard Ethernet Drivers from $GHURL" -Verbose
 Write-Host
 
 Save-WebFile -SourceUrl $TaichiEthernetURL -DestinationDirectory $TaichiDestination
-
 
 Write-Verbose "Processing: Downloading ASRock LGA 1700 Motherboard Storage Drivers from $GHURL" -Verbose
 Write-Host
@@ -62,18 +50,15 @@ Write-Host
 
 Save-WebFile -SourceUrl $TaichiWLANURL -DestinationDirectory $TaichiDestination
 
-
 Write-Verbose "Completed: ASRock LGA1700 Taichi Motherboard Drivers downloaded" -Verbose
 Write-Host
 
 Write-Verbose "Processing: ZIP File extraction" -Verbose
 Write-Host
 
-Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-LGA1700-Taichi-BluetoothDrivers.zip" -TargetPath $TaichiBluetooth -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-LGA1700-Taichi-Intel-EthernetDrivers.zip" -TargetPath $TaichiEthernet  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-LGA1700-Taichi-IRST-StorageDrivers.zip" -TargetPath $TaichiStorage  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$TaichiDestination\ASRock-LGA1700-Taichi-Intel-Killer-WLANDrivers.zip" -TargetPath $TaichiWLAN  -ErrorAction SilentlyContinue 
-
 
 Write-Verbose "Completed: ZIP Files extracted successfully" -Verbose
 Write-Host
