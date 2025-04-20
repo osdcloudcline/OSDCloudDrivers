b6ca6713fd5057cae47d7245bd2abeaea2f968ec
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-19 23:39:53 -0400

    Update PhantomGaming.ps1

--- Network and Storage /ASRock/Desktops/Intel/LGA 1700/Phantom Gaming Motherboards/PhantomGaming.ps1
+++ Network and Storage /ASRock/Desktops/Intel/LGA 1700/Phantom Gaming Motherboards/PhantomGaming.ps1
@@ -12,9 +12,6 @@ Import-Module -Name 7Zip4Powershell -Force
 #          LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver URLs
 ####################################################################################
 
-
-$PhantomGamingBluetoothURL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Phantom%20Gaming%20Motherboards/Bluetooth/ASRock-LGA1700-PhantomGaming-BluetoothDrivers1.zip"
-
 $PhantomGamingEthernet1URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Phantom%20Gaming%20Motherboards/Ethernet/ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers1.zip"
 $PhantomGamingEthernet2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Phantom%20Gaming%20Motherboards/Ethernet/ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers2.zip"
 $PhantomGamingEthernet3URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Phantom%20Gaming%20Motherboards/Ethernet/ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers3.zip"
@@ -30,13 +27,11 @@ $PhantomGamingWLAN2URL = "https://github.com/osdcloudcline/OSDCloudDrivers/raw/r
 #    LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver Download Destination
 ####################################################################################
 
-$PhantomGamingDestination = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming"
+$PhantomGamingDestination = "C:\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming"
 
 ####################################################################################
 #   LGA 1700 Bluetooth, Ethernet, Storage and WLAN Driver Extraction Destination
 ####################################################################################
-
-$PhantomGamingBluetooth = "$PhantomGamingDestination\Bluetooth"
 
 $PhantomGamingEthernet1 = "$PhantomGamingDestination\Ethernet1"
 $PhantomGamingEthernet2 = "$PhantomGamingDestination\Ethernet2"
@@ -48,11 +43,6 @@ $PhantomGamingStorage = "$PhantomGamingDestination\Storage"
 
 $PhantomGamingWLAN1 = "$PhantomGamingDestination\WLAN1"
 $PhantomGamingWLAN2 = "$PhantomGamingDestination\WLAN2"
-
-Write-Verbose "Processing: Downloading ASRock LGA 1700 Motherboard Bluetooth Drivers from $GHURL" -Verbose
-Write-Host
-
-Save-WebFile -SourceUrl $PhantomGamingBluetoothURL -DestinationDirectory $PhantomGamingDestination
 
 Write-Verbose "Processing: Downloading ASRock LGA 1700 Motherboard Ethernet Drivers from $GHURL" -Verbose
 Write-Host
@@ -80,14 +70,12 @@ Write-Host
 Write-Verbose "Processing: ZIP File extraction" -Verbose
 Write-Host
 
-Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-BluetoothDrivers1.zip" -TargetPath $PhantomGamingBluetooth -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers1.zip" -TargetPath $PhantomGamingEthernet1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers2.zip" -TargetPath $PhantomGamingEthernet2 -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers3.zip" -TargetPath $PhantomGamingEthernet3  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Intel-EthernetDrivers4.zip" -TargetPath $PhantomGamingEthernet4 -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Realtek-EthernetDrivers5.zip" -TargetPath $PhantomGamingEthernet5 -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-IRST-StorageDrivers.zip" -TargetPath $PhantomGamingStorage  -ErrorAction SilentlyContinue 
-
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Intel-Killer-WLANDrivers.zip" -TargetPath $PhantomGamingWLAN1  -ErrorAction SilentlyContinue 
 Expand-7Zip -ArchiveFileName "$PhantomGamingDestination\ASRock-LGA1700-PhantomGaming-Mediatek-WLANDrivers.zip" -TargetPath $PhantomGamingWLAN2  -ErrorAction SilentlyContinue 
 
