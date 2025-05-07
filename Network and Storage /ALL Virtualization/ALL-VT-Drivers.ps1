bc9203a67ef72d3a22852c0077423c25aa513ee6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-07 01:10:51 -0400

    Update ALL-VT-Drivers.ps1

--- Network and Storage /ALL Virtualization/ALL-VT-Drivers.ps1
+++ Network and Storage /ALL Virtualization/ALL-VT-Drivers.ps1
@@ -1,6 +1,24 @@
-Write-Verbose "Processing: Microsoft Hyper-V Drivers" -Verbose
+Write-Verbose "Processing: Microsoft Hyper-V Virtualization Drivers" -Verbose
 
 $HyperVDrivers = Invoke-WebRequest("")
 Invoke-Expression $($HyperVDrivers.Content)
 
+Write-Verbose "Processing: Proxmox Virtualization Drivers..." -Verbose
 
+$ProxmoxDrivers = Invoke-WebRequest("")
+Invoke-Expression $($ProxmoxDrivers.Content)
+
+Write-Verbose "Processing: VMWare ESXI Virtualization Drivers" -Verbose
+
+$ESXIDrivers = Invoke-WebRequest("")
+Invoke-Expression $($ESXIDrivers.Content)
+
+Write-Verbose "Processing: VMWare Workstation Professional Virtualization Drivers..." -Verbose
+
+$VMWareWSProDrivers = Invoke-WebRequest("")
+Invoke-Expression $($VMWareWSProDrivers.Content)
+
+Write-Verbose "Processing: unRAID NAS Virtualization Drivers..." -Verbose
+
+$unRAIDDrivers = Invoke-WebRequest("")
+Invoke-Expression $($unRAIDDrivers.Content)
